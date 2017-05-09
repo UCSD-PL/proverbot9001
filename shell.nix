fbec14fbc7394b941cf52bb3cf7ad1d92ae40d57
Valentin Robert <valentin.robert.42@gmail.com>
2017-05-09 13:43:54 -0700

    added shell.nix

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+nixpkgs.stdenv.mkDerivation {
+  name = "proverbot9001";
+  buildInputs = (with nixpkgs; [
+  ] ++ (with ocamlPackages_4_02; [
+      # Coq:
+      camlp5_6_strict
+      findlib
+      ocaml
+      # CoqIDE:
+      lablgtk
+      # SerAPI:
+      camlp4
+      cmdliner
+      ocamlbuild
+      opam
+      ppx_import
+      ppx_sexp_conv
+      sexplib
+    ])
+  );
+  nativeBuildInputs = (with nixpkgs; [
+  ]);
+  shellHook = ''
+    export NIXSHELL="$NIXSHELL\[proverbot9001\]"
+    export SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt"
+    export SERAPI_COQ_HOME="/home/ptival/proverbot9001/coq/"
+  '';
+}
