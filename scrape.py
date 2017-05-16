4ac8b97b33c7176903ee4f640ccfab3dec9e5b18
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2017-05-15 17:13:49 -0700

    Make scraper more robust

--- scrape.py
+++ scrape.py
@@ -10,6 +10,24 @@ import sys
 # This dependency is in pip, the python package manager
 from sexpdata import *
 
+class AckError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    pass
+class CompletedError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    pass
+
+class CoqExn(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    pass
+class BadResponse(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    pass
+
 class SerapiInstance(threading.Thread):
     def __init__(self, coq_command, includes):
         threading.Thread.__init__(self, daemon=True)
@@ -22,25 +40,30 @@ class SerapiInstance(threading.Thread):
         self.exec_includes(includes)
 
     def run_stmt(self, stmt):
+        stmt = stmt.replace("\\", "\\\\")
         stmt = stmt.replace("\"", "\\\"")
-        print("Running command \"{}\"".format(stmt))
-        self._fin.write("(Control (StmAdd () \"{}\"))\n".format(stmt).encode('utf-8'))
-        self._fin.flush()
-        next_state = self.get_next_state()
-        self._fin.write("(Control (StmObserve {}))\n".format(next_state).encode('utf-8'))
-        self._fin.flush()
-        feedbacks = self.get_feedbacks()
-        # print(feedbacks)
+        try:
+            self._fin.write("(Control (StmAdd () \"{}\"))\n".format(stmt).encode('utf-8'))
+            self._fin.flush()
+            next_state = self.get_next_state()
+            self._fin.write("(Control (StmObserve {}))\n".format(next_state).encode('utf-8'))
+            self._fin.flush()
+            feedbacks = self.get_feedbacks()
+        except Exception as e:
+            print("Problem running statement: {}".format(stmt))
+            raise e
 
     def get_ack(self):
         ack = self.messages.get()
-        assert ack[0] == Symbol("Answer")
-        assert ack[2] == Symbol("Ack")
+        if (ack[0] != Symbol("Answer") or
+            ack[2] != Symbol("Ack")):
+            raise AckError(ack)
 
     def get_completed(self):
         completed = self.messages.get()
-        assert completed[0] == Symbol("Answer")
-        assert completed[2] == Symbol("Completed")
+        if (completed[0] != Symbol("Answer") or
+            completed[2] != Symbol("Completed")):
+            raise CompletedError(completed)
 
     def add_lib(self, origpath, logicalpath):
         addStm = ("(Control (StmAdd () \"Add Rec LoadPath \\\"{}\\\" as {}.\"))\n"
@@ -59,23 +82,24 @@ class SerapiInstance(threading.Thread):
         msg = self.messages.get()
         while msg[0] == Symbol("Feedback"):
             msg = self.messages.get()
-        assert msg[0] == Symbol("Answer")
+        if (msg[0] != Symbol("Answer")):
+            raise BadResponse(msg)
         submsg = msg[2]
         if (submsg[0] == Symbol("CoqExn")):
-            print("Error!")
-            print(msg)
-            assert False
+            raise CoqExn(submsg)
+        elif submsg[0] != Symbol("StmAdded"):
+            raise BadResponse(submsg)
         else:
-            assert submsg[0] == Symbol("StmAdded")
             state_num = submsg[1]
             self.get_completed()
             return state_num
 
     def discard_initial_feedback(self):
         feedback1 = self.messages.get()
-        assert feedback1[0] == Symbol("Feedback")
         feedback2 = self.messages.get()
-        assert feedback2[0] == Symbol("Feedback")
+        if (feedback1[0] != Symbol("Feedback") or
+            feedback2[0] != Symbol("Feedback")):
+            raise BadResponse("Not feedback")
 
     def get_feedbacks(self):
         self.get_ack()
@@ -86,13 +110,12 @@ class SerapiInstance(threading.Thread):
             feedbacks.append(next_message)
             next_message = self.messages.get()
         fin = next_message
-        assert fin[0] == Symbol("Answer")
+        if (fin[0] != Symbol("Answer")):
+            raise BadResponse(fin)
         if (isinstance(fin[2], list)):
-            print("Error!")
-            print(fin)
-            assert False
-        else:
-            assert fin[2] == Symbol("Completed")
+            raise BadResponse(fin)
+        elif(fin[2] != Symbol("Completed")):
+            raise BadResponse(fin)
 
         return feedbacks
 
@@ -111,6 +134,57 @@ class SerapiInstance(threading.Thread):
         threading.Thread.join(self)
 
     pass
+
+num_jobs = 0
+jobs = queue.Queue()
+workers = []
+
+class Worker(threading.Thread):
+    def __init__(self, output):
+        threading.Thread.__init__(self)
+        self.coqargs = [os.path.dirname(os.path.abspath(__file__)) + "/coq-serapi/sertop.native",
+                   "--prelude={}/coq".format(os.path.dirname(os.path.abspath(__file__)))]
+        includes=subprocess.Popen(['make', 'print-includes'], stdout=subprocess.PIPE).communicate()[0]
+        self.includes=includes.strip().decode('utf-8')
+        if output == None:
+            self.fout = sys.stdout
+        else:
+            self.fout = open(args.output, 'w')
+        self.coq= None
+        pass
+
+    def kill(self):
+        if (self.coq != None):
+            self.coq.kill()
+        threading.Thread.join(self)
+
+    def run(self):
+        try:
+            while(True):
+                job = jobs.get_nowait()
+                print("Processing file {} ({} of {})".format(job,
+                                                             num_jobs - jobs.qsize(),
+                                                             num_jobs))
+                with open(job, 'r') as fin:
+                    contents = kill_comments(fin.read())
+                    self.coq = SerapiInstance(self.coqargs, self.includes)
+                    commands = [newcmd for cmd in split_commands(contents)
+                                for newcmd in preprocess_command(cmd)]
+                    try:
+                        for command in commands:
+                            self.coq.run_stmt(command)
+                    except Exception as e:
+                        print("In file {}:".format(job))
+                        raise e
+                    self.coq.kill()
+        except queue.Empty:
+            pass
+        except Exception as e:
+            for worker in workers:
+                if worker == self:
+                    continue
+                worker.kill()
+            raise e
 
 def kill_comments(string):
     result = ""
@@ -144,9 +218,16 @@ def split_commands(string):
         else:
             if string[i] == '"' and string[i-1] != '\\':
                 in_quote = True
-            if (re.match("[\{\}\+\-\*]", string[i]) and
+            if (re.match("[\{\}]", string[i]) and
                 re.fullmatch("\s*", next_command)):
                 result.append(string[i])
+                next_command = ""
+                continue
+            if (re.match("[\+\-\*]", string[i]) and
+                string[i] != string[i+1] and
+                re.fullmatch("\s*[\+\-\*]*", next_command)):
+                next_command += string[i]
+                result.append(next_command.strip())
                 next_command = ""
                 continue
             if (re.match("\.\s", string[i:i+2]) and
@@ -157,31 +238,69 @@ def split_commands(string):
         next_command += string[i]
     return result
 
+def preprocess_command(cmd):
+    needPrefix = ["String", "Classical", "ClassicalFacts",
+                  "ClassicalDescription", "ClassicalEpsilon",
+                  "Equivalence", "Init.Wf", "Program.Basics",
+                  "Max", "Wf_nat", "EquivDec", "Znumtheory",
+                  "Bool", "Zquot", "FSets", "FSetAVL",
+                  "Wellfounded", "FSetInterface",
+                  "OrderedType", "Program", "Recdef", "Eqdep_dec",
+                  "FunctionalExtensionality", "Zwf", "Permutation",
+                  "Orders", "Mergesort", "List", "ZArith", "Int31",
+                  "Syntax", "Streams", "Equality",
+                  "ProofIrrelevance", "Setoid", "EqNat",
+                  "Arith", "Cyclic31", "Omega", "Relations",
+                  "RelationClasses", "OrderedTypeAlt", "FMapAVL",
+                  "BinPos", "BinNat", "DecidableClass", "Reals",
+                  "Psatz", "ExtrOcamlBasic", "ExtrOcamlString",
+                  "Ascii"]
+    for lib in needPrefix:
+        match = re.fullmatch("\s*Require(\s+(?:(?:Import)|(?:Export)))?((?:\s+\S+)*)\s+({})\s*((?:\s+\S*)*)\.".format(lib), cmd)
+        if match:
+            if match.group(1):
+                impG= match.group(1)
+            else:
+                impG=""
+            if match.group(4):
+                after = match.group(4)
+            else:
+                after=""
+            if (re.fullmatch("\s*", match.group(2)) and re.fullmatch("\s*", after)):
+                return ["From Coq Require" + impG + " " + match.group(3) + "."]
+            else:
+                return ["From Coq Require" + impG + " " + match.group(3) + "."] + preprocess_command("Require " + impG.strip() + " " + match.group(2).strip() + " " + after + ".")
+    return [cmd]
+
 def process_file(fin, fout, coqargs, includes):
     coq = SerapiInstance(coqargs, includes)
     contents = fin.read()
     contents = kill_comments(contents)
     commands = split_commands(contents)
+    commands = [newcmd for cmd in commands for newcmd in preprocess_command(cmd)]
     for command in commands:
         coq.run_stmt(command)
     coq.kill()
 
-coqargs = [os.path.dirname(os.path.abspath(__file__)) + "/coq-serapi/sertop.native",
-           "--prelude={}/coq".format(os.path.dirname(os.path.abspath(__file__)))]
-includes=subprocess.Popen(['make', 'print-includes'], stdout=subprocess.PIPE).communicate()[0]
-includes=includes.strip().decode('utf-8')
-
 parser = argparse.ArgumentParser(description="scrape a proof")
 parser.add_argument('-o', '--output', help="output data file name", default=None)
+parser.add_argument('-j', '--threads', default=1, type=int)
 parser.add_argument('inputs', nargs="+", help="proof file name(s) (*.v)")
 args = parser.parse_args()
 
-if args.output == None:
-    fout = sys.stdout
-else:
-    fout = open(args.output, 'w')
+num_jobs = len(args.inputs)
 
-for idx, infname in enumerate(args.inputs):
-    print("Processing file {} ({} of {})".format(infname, idx + 1, len(args.inputs)))
-    with open(infname) as fin:
-        process_file(fin, fout, coqargs, includes)
+for infname in args.inputs:
+    jobs.put(infname)
+
+for idx in range(args.threads):
+    worker = Worker(args.output)
+    worker.start()
+    workers.append(worker)
+
+try:
+    for worker in workers:
+        worker.join()
+except:
+    for worker in workers:
+        worker.kill()
