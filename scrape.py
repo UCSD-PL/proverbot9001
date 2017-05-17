29d1558b1c40c2b6befb6c0eb4695e45ae433d9e
Valentin Robert <valentin.robert.42@gmail.com>
2017-05-16 22:44:29 -0700

    added linearization of semicolons

--- scrape.py
+++ scrape.py
@@ -28,6 +28,122 @@ class BadResponse(Exception):
         self.msg = msg
     pass
 
+def split_semis_carefully(string):
+    stack = 0
+    item = []
+    for char in string:
+        if char == ';' and stack == 0:
+            yield ''.join(item)
+            item = []
+            continue
+        item.append(char)
+        # if we are entering a block
+        pos = '(['.find(char)
+        if pos >= 0:
+            stack += 1
+            continue
+        pos = ')]'.find(char)
+        if pos >= 0:
+            stack -= 1
+    if stack != 0:
+        raise ValueError("unclosed parentheses: %s" % ''.join(item))
+    if item:
+        yield ''.join(item)
+
+# split_semis_brackets : String -> [[String]]
+# "a ; [b | c | d]; e[.]"
+# becomes
+# [ a , [ b , c , d ], e ]
+# Note that b, c, d may still be of the shape "foo ; [ bar | baz ]"
+def split_semis_brackets(s):
+    s = s.rstrip(' .')
+    #print('SPLITTING: ' + str(s))
+    semiUnits = list(split_semis_carefully(s.rstrip(' .')))
+    #print("semiUnits :" + str(semiUnits))
+    def unbracket(s):
+        s = s.lstrip(' ')
+        if s.startswith('['): return s.lstrip(' [').rstrip(' ]').split('|')
+        else:                 return [s]
+    return list(map(unbracket, semiUnits))
+
+def count_open_proofs(coq):
+    return len(coq.query_goals()[2][1])
+
+def count_fg_goals(coq):
+    return len(coq.query_goals()[2][1][0][1][0][1])
+
+# semiands   : [[String]]
+# ksemiands  : [[[String]]]
+# periodands : [String]
+# done       : Int
+
+# Note: each tactic has a copy of ksemiands they can mutate
+# Note: all tactics share the same periodand
+def linearize(coq, fout, semiands, ksemiands, periodands, done):
+
+    nb_goals_before = count_fg_goals(coq)
+    #print("CURRENT GOAL COUNT: " + str(nb_goals_before) + ", done: " + str(done))
+
+    if nb_goals_before == done:
+        #print("DONE WITH THIS PATH")
+        return
+
+    #print("LINEARIZING " + str(semiands) + ", done: " + str(done))
+
+    # if done with the current semicolon periscope
+    if len(semiands) == 0:
+        if len(ksemiands) != 0:
+            #print("POPPING NEXT KSEMIAND")
+            nextTactic = ksemiands.pop(0)
+            return linearize(coq, fout, split_semis_brackets(nextTactic), ksemiands, periodands, done)
+        else:
+            if len(periodands) != 0:
+                #print("POPPING NEXT PERIODAND")
+                nextTactic = periodands.pop(0)
+                return linearize(coq, fout, split_semis_brackets(nextTactic), [], periodands, done)
+            else:
+                print("ERROR: ran out of tactic w/o finishing the proof")
+                return
+
+    #print(str(semiands))
+    semiand = semiands.pop(0)
+
+    # if the current semiand is a dispatch
+    # [ a | b | c ] or [ a | .. b .. | c ]
+    if len(semiand) > 1:
+        #print("DETECTED DISPATCH, length " + str(len(semiand)))
+        ksemiandsCopy = ksemiands[:] # pass a copy to each subgoal
+        ksemiandsCopy.append(semiands[:])
+        for i in range(len(semiand)):
+            # Say the tactic was `[ a | b | c] ; d`
+            # Subgoal 0 is done when `done`
+            # Subgoal 1 is done when `done-1`
+            # Subgoal 2 is done when `done-2`
+            # Subgoal i is done when `done - i`
+            new_semiands = list(split_semis_brackets(semiand[i]))
+            linearize(coq, fout, new_semiands, ksemiandsCopy, periodands, done - i)
+        return
+
+    # if the current tactic is not a dispatch, run it?
+    tactic = semiand[0]
+    coq.run_stmt(tactic + '.')
+    #print('>>> ' + tactic + '.')
+    fout.write(tactic + '.\n')
+
+    nb_goals_after = count_fg_goals(coq)
+    nb_subgoals = 1 + nb_goals_after - nb_goals_before
+    #print("Goal difference: " + str(nb_goals_difference))
+
+    semiandsCopy = semiands[:] # pass a copy to each subgoal
+    for i in range(nb_subgoals):
+        #print("LOOP ITERATION " + str(i))
+        # Say the tactic was `a; b` and `a` generates 3 subgoals
+        # Subgoal 0 is done when `done+2`
+        # Subgoal 1 is done when `done+1`
+        # Subgoal 2 is done when `done`
+        # Subgoal i is done when `done + nb_subgoals - (i + 1)`
+        linearize(coq, fout, semiandsCopy[:], [], periodands, done + nb_subgoals - (i + 1))
+
 class SerapiInstance(threading.Thread):
     def __init__(self, coq_command, includes):
         threading.Thread.__init__(self, daemon=True)
@@ -119,6 +235,14 @@ class SerapiInstance(threading.Thread):
 
         return feedbacks
 
+    def query_goals(self):
+        query = "(Query () Goals)\n"
+        self._fin.write(query.encode('utf-8'))
+        self._fin.flush()
+        self.get_ack()
+        answer = self.messages.get()
+        return answer
+
     def run(self):
         while(True):
             try:
@@ -158,10 +282,35 @@ class Worker(threading.Thread):
             self.coq.kill()
         threading.Thread.join(self)
 
+    def linearize(self, job):
+        with open(job, 'r') as fin:
+            job_filename, job_extension = os.path.splitext(job)
+            with open(job_filename + ".linear.v", 'w') as fout:
+                contents = kill_comments(fin.read())
+                self.coq = SerapiInstance(self.coqargs, self.includes)
+                commands = [newcmd for cmd in split_commands(contents)
+                                for newcmd in preprocess_command(cmd)]
+                while commands:
+                    command = commands.pop(0)
+                    #print("POPPED: " + command)
+
+                    if count_open_proofs(self.coq) == 0 or count_fg_goals(self.coq) == 0:
+                        self.coq.run_stmt(command)
+                        #print('>>> ' + command)
+                        fout.write(command + '\n')
+                        continue
+
+                    #print("TIME TO LINEARIZE: " + command)
+                    linearize(self.coq, fout, split_semis_brackets(command), [], commands, 0)
+
+                self.coq.kill()
+
+
     def run(self):
         try:
             while(True):
                 job = jobs.get_nowait()
+                self.linearize(job)
                 print("Processing file {} ({} of {})".format(job,
                                                              num_jobs - jobs.qsize(),
                                                              num_jobs))
