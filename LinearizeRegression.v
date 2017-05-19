5df5641518d9527b30af0d87824b32b9a700bcca
Valentin Robert <valentin.robert.42@gmail.com>
2017-05-18 17:23:18 -0700

    updated LinearizeRegression.v

--- LinearizeRegression.v
+++ LinearizeRegression.v
@@ -5,9 +5,27 @@ Proof.
   intros; destruct a; intuition; destruct b; intuition.
 Qed.
 
-Theorem linerarizeMeHard : forall a b, a = true \/ b && a = false.
+Theorem linerarizeMeMedium : forall a b, a = true \/ b && a = false.
 Proof.
   intros [|]; [ intros; left | intros ].
   reflexivity.
   right; destruct b; reflexivity.
+Qed.
+
+Ltac break_and :=
+  match goal with
+  | [ H : _ /\ _ |- _ ] => destruct H
+  end.
+
+Theorem linerarizeMeHard :
+  forall (A B C D E F : Prop)
+    (ABC: A -> B /\ C)
+    (BD: B -> D)
+    (CE: C -> E)
+    (DEF: D /\ E -> F)
+    (a : A), F.
+Proof with (split; [ apply BD | apply CE ]; assumption).
+  intros.
+  destruct (ABC a).
+  apply DEF...
 Qed.
