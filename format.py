1ff46bc2c2a776d0d23442144df168ee46fdf331
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2017-05-30 20:39:56 -0700

    Minimize whitespace

--- format.py
+++ format.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import re
+
+def minimize_whitespace(data):
+    return re.sub("\s+", " ", data)
+
 def format_command_record(prev_tactics, prev_hyps, prev_goal,
                           tactic, post_hyps, post_goal):
     buf = ""
     buf += "*****\n"
-    buf += prev_goal + "\n"
+    buf += minimize_whitespace(prev_goal) + "\n"
     buf += "+++++\n"
-    buf += tactic + "\n"
+    buf += minimize_whitespace(tactic) + "\n"
     return buf
