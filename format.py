ce605aa50881d2aaebd87c1ae323c9d3c845b5e5
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2017-07-06 09:56:49 -0700

    Strip whitespace in formatter

--- format.py
+++ format.py
@@ -3,7 +3,7 @@
 import re
 
 def minimize_whitespace(data):
-    return re.sub("\s+", " ", data)
+    return re.sub("\s+", " ", data).strip()
 
 def format_context(prev_tactics, prev_hyps, prev_goal):
     return minimize_whitespace(prev_goal) + "\n+++++\n"
