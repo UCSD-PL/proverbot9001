53c7d5923fac1ae3bc4c974452994fe70dd4f07f
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2017-07-10 18:19:04 -0700

    Set temperture parameter in darknet predictor script

--- darknet-rnn-predictor.sh
+++ darknet-rnn-predictor.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cd darknet && ./darknet rnn generatetactic cfg/coq1.cfg ~/backup/coq.backup
+cd darknet && ./darknet rnn generatetactic cfg/coq1.cfg ~/backup/coq.backup -temp .4
