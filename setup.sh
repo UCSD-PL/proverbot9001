625dbf430325b8ba7eae774ea2697116669a7b9a
Valentin Robert <valentin.robert.42@gmail.com>
2017-07-14 15:51:05 -0700

    setup.sh: do not configure CompCert every time

--- setup.sh
+++ setup.sh
@@ -59,7 +59,9 @@ function setup-compcert {
     {
         set -euv
         cd CompCert
-        PATH="$PWD/../coq/bin:$PATH" ./configure x86_64-linux
+        if [[ ! -f "Makefile.config" ]]; then
+            PATH="$PWD/../coq/bin:$PATH" ./configure x86_64-linux
+        fi
         PATH="$PWD/../coq/bin:$PATH" make -j `nproc`
     }
 }
