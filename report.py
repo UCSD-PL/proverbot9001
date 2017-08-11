b73f32089b2d082cab8e84ba3dae326529dfba8a
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2017-08-11 09:26:36 -0700

    Add milliseconds to report

--- report.py
+++ report.py
@@ -427,7 +427,7 @@ with tag('html'):
         with tag('h5'):
             text("Commit: {}".format(cur_commit))
         with tag('h5'):
-            text("Run on {}".format(cur_date.strftime("%Y-%m-%d %H:%M:%S %Z")))
+            text("Run on {}".format(cur_date.strftime("%Y-%m-%d %H:%M:%S.%f")))
         gresult.report_results(doc, text, tag, line)
 
 extra_files = ["report.css", "details.css", "details.js"]
