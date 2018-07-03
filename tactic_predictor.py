0de6b9a69e16116b870288c4c9496541cc5716d9
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2018-07-03 10:57:11 -0700

    Inteface for tactic predictors

---
+++ tactic_predictor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from typing import Dict, List
+
+class TacticPredictor:
+    def __init__(self, options : Dict[str, int]) -> None:
+        pass
+    def predictKTactics(self, in_data : Dict[str, str], k : int) -> List[str]:
+        pass
