48bbd728950ab56a73ba1a2c2d9d5fea7cd9fa0e
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2018-07-06 15:08:18 -0700

    Refactor works now

--- predict_tactic.py
+++ predict_tactic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Dict, List
+from typing import Dict, List, Union
 import encdecrnn_predictor
 from tactic_predictor import TacticPredictor
 
@@ -8,5 +8,5 @@ predictors = {
     'encdecrnn' : encdecrnn_predictor.EncDecRNNPredictor
 }
 
-def loadPredictor(options : Dict[str, int], predictor_type="encdecrnn") -> TacticPredictor:
+def loadPredictor(options : Dict[str, Union[int, str]], predictor_type="encdecrnn") -> TacticPredictor:
     return predictors[predictor_type](options)
--- tactic_predictor.py
+++ tactic_predictor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 class TacticPredictor:
-    def __init__(self, options : Dict[str, int]) -> None:
+    def __init__(self, options : Dict[str, Union[int, str]]) -> None:
         pass
     def predictKTactics(self, in_data : Dict[str, str], k : int) -> List[str]:
+        assert False, "You can't predict on the base class!"
         pass
