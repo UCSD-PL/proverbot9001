eceef6cebca667b11950882cfac842753060fbed
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-07-26 15:13:19 -0700

    Add try-common predictor
    This predictor determines the most common tactics in the file, and
    tries them.

--- predict_tactic.py
+++ predict_tactic.py
@@ -4,9 +4,11 @@ from typing import Dict, List, Union
 from models.tactic_predictor import TacticPredictor
 
 from models import encdecrnn_predictor
+from models import try_common_predictor
 
 predictors = {
     'encdecrnn' : encdecrnn_predictor.EncDecRNNPredictor,
+    'trycommon' : try_common_predictor.TryCommonPredictor,
 }
 
 def loadPredictor(options : Dict[str, Union[int, str]], predictor_type) -> TacticPredictor:
