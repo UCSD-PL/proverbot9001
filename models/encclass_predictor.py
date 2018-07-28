4eb40b820cceedaad61ca67698888dd9d6b770ce
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-07-28 15:40:38 -0700

    Decreased default learning rate

--- models/encclass_predictor.py
+++ models/encclass_predictor.py
@@ -186,7 +186,7 @@ def take_args(args) -> Tuple[str, Any]:
     parser.add_argument("--print-every", dest="print_every", default=10, type=int)
     parser.add_argument("--hidden-size", dest="hidden_size", default=128, type=int)
     parser.add_argument("--learning-rate", dest="learning_rate",
-                        default=.6, type=float)
+                        default=.4, type=float)
     parser.add_argument("--num-encoder-layers", dest="num_encoder_layers",
                         default=3, type=int)
     return parser.parse_args(args)
