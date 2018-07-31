c0b62add91c93186582a35c49b3b4b2536f8376c
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-07-31 15:38:33 -0700

    Add script for enqueuing training tasks

---
+++ queue-test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -e
+
+# determine physical directory of this script
+src="${BASH_SOURCE[0]}"
+while [ -L "$src" ]; do
+  dir="$(cd -P "$(dirname "$src")" && pwd)"
+  src="$(readlink "$src")"
+  [[ $src != /* ]] && src="$dir/$src"
+done
+MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
+
+if [ $# -eq 0 ]
+then
+    echo "Not enough arguments! Must supply predictor"
+    exit 1
+fi
+
+weights=$(mktemp /tmp/proverbot-weights.XXX)
+outdir=$(mktemp -d $PWD/report-XXX)
+
+echo "Saving to weights $weights"
+echo "Saving report to $outdir"
+
+tsp -fn ./proverbot9001.py train "$@" scrape.txt "$weights" && make FLAGS=--predictor="$1 -o $outdir" report
