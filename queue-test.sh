468163e8b8f1f1e09190f82873534ab831b3ea57
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-07-31 17:54:05 -0700

    Use a specialized tsp socket

--- queue-test.sh
+++ queue-test.sh
@@ -24,6 +24,7 @@ outdir=$(mktemp -d $PWD/report-XXX)
 echo "Saving to weights $weights"
 echo "Saving report to $outdir"
 
+export TS_SOCKET=/tmp/graphicscard
 
 tsp -fn ./proverbot9001.py train "$@" scrape.txt "$weights" && make FLAGS=--predictor="$1 -o $outdir --weightsfile=$weights" report
 
