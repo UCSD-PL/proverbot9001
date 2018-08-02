efd96c2d2c2cd8124d83c580cc828833ea224aa6
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-08-02 13:22:28 -0700

    Fix syntax highlighting in reports?

--- syntax.py
+++ syntax.py
@@ -78,8 +78,8 @@ def highlight_comments(page : str) -> str:
 
 def syntax_highlight(page : str) -> str:
     for vernac in vernacular_words:
-        page = re.sub(vernac,
-                      color_word(vernacular_color, vernac),
+        page = re.sub("(\s+)" + vernac + "(\s+)",
+                      "\1" + color_word(vernacular_color, vernac) + "\2",
                       page)
     return highlight_comments(page);
 
