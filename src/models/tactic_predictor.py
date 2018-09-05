6bb99fee2caf09a115425481c3d13e69e3a74510
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-09-04 18:11:11 -0700

    Make TacticPredictor a proper abstract base class

--- src/models/tactic_predictor.py
+++ src/models/tactic_predictor.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 from typing import Dict, List, Union, Tuple
+from abc import ABCMeta, abstractmethod
 
-class TacticPredictor:
-    def getOptions(self) -> List[Tuple[str, str]]:
-        assert False, "Need to override getOptions!"
-        pass
+class TacticPredictor(metaclass=ABCMeta):
+    @abstractmethod
+    def getOptions(self) -> List[Tuple[str, str]]: pass
+
     def __init__(self, options : Dict[str, Union[int, str]]) -> None:
         pass
-    def predictKTactics(self, in_data : Dict[str, str], k : int) -> List[str]:
-        assert False, "You can't predict on the base class!"
-        pass
+    @abstractmethod
+    def predictKTactics(self, in_data : Dict[str, str], k : int) \
+        -> List[Tuple[str, float]]: pass
+    @abstractmethod
     def predictKTacticsWithLoss(self, in_data : Dict[str, str],
                                 k : int, correct : str) -> \
-        Tuple[List[str], float]:
-        assert False, "You can't predict on the base class!"
-        pass
+        Tuple[List[Tuple[str, float]], float]: pass
