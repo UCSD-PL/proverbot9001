8818ab04a187fe01110bc50922d8f41b23dfd7c5
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-09-25 16:33:42 -0700

    Some WIP on encstruct predictor

--- src/models/encstruct_predictor.py
+++ src/models/encstruct_predictor.py
@@ -4,16 +4,18 @@ import argparse
 import time
 import threading
 from typing import Dict, List, Union, Any, Tuple, Iterable, cast, Callable
+from itertools import takewhile
 
 from models.encdecrnn_predictor import inputFromSentence
 from tokenizer import Tokenizer, tokenizers, get_symbols
 from models.tactic_predictor import TacticPredictor
-from models.args import take_std_args, optimizers
+from models.args import add_std_args, optimizers
 from models.components import SimpleEmbedding
 from context_filter import get_context_filter
 
 from data import read_text_data, filter_data, RawDataset, make_keyword_tokenizer, Sentence
 from util import *
+from util import _inflate
 import serapi_instance
 
 import torch
@@ -25,15 +27,6 @@ import torch.optim.lr_scheduler as scheduler
 import torch.nn.functional as F
 import torch.utils.data as data
 import torch.cuda
-
-no_args_tactics = ["auto", "eauto", "constructor", "econstructor",
-                   "simpl", "intro", "intros", "xomega", "simpl",
-                   "intro", "intros"]
-ind_star_args_tactics = ["induction", "einduction", "intros until"]
-star_args_tactics = ["subst"]
-one_arg_tactics = ["inv", "inversion", "exists", "induction",
-                   "rewrite", "erewrite", "rewrite <-", "erewrite <-",
-                   "apply", "eapply", "simpl in", "destruct"]
 
 TacticStructure = Tuple[int, List[int]]
 StructDataset = List[Tuple[List[Sentence], Sentence, TacticStructure]]
@@ -47,14 +40,32 @@ class EncStructPredictor(TacticPredictor):
         assert checkpoint['context-filter']
         assert checkpoint['max-length']
         assert checkpoint['optimizer']
+        assert checkpoint['num-encoder-layers']
+        assert checkpoint['num-decoder-layers']
+        assert checkpoint['encoder']
+        assert checkpoint['stem-decoder']
+        assert checkpoint['arg-decoder']
 
         self.options = [("tokenizer", checkpoint["tokenizer-name"]),
                         ("optimizer", checkpoint["optimizer-name"]),
-                        ("context filter", checkpoint["context-filter"])]
+                        ("context filter", checkpoint["context-filter"]),
+                        ("# encoder layers", checkpoint["num-encoder-layers"]),
+                        ("# decoder layers", checkpoint["num-decoder-layers"])]
 
         self.tokenizer = checkpoint['tokenizer']
         self.embedding = checkpoint['embedding']
         self.max_length = checkpoint["max-length"]
+        self.encoder = maybe_cuda(RNNEncoder(self.tokenizer.numTokens(),
+                                             checkpoint['hidden-size'],
+                                             checkpoint['num-encoder-layers']))
+        self.encoder.load_state_dict(checkpoint['encoder'])
+        self.stem_decoder = maybe_cuda(StemClassifier(checkpoint['hidden-size'],
+                                                      self.embedding.num_tokens(),
+                                                      checkpoint['num-decoder-layers']))
+        self.stem_decoder.load_state_dict(checkpoint['stem-decoder'])
+        self.arg_decoder = maybe_cuda(ArgumentDecoder(checkpoint['hidden-size'],
+                                                      checkpoint['num-decoder-layers']))
+        self.arg_decoder.load_state_dict(checkpoint['arg-decoder'])
         pass
     def __init__(self, options : Dict[str, Any]) -> None:
         assert(options["filename"])
@@ -79,16 +90,16 @@ class EncStructPredictor(TacticPredictor):
                                          structure, cast(List[str], in_data["hyps"])),
                  certainty)
                 for structure, certainty in zip(prediction_structures, certainties)]
-    def decodeKTactics(self, encoded_vector : torch.LongTensor, k : int,
+    def decodeKTactics(self, encoded_vector : torch.FloatTensor, k : int,
                        hyps : List[str],
                        beam_width : int, max_args : int) -> \
                        Tuple[List[TacticStructure], List[float]]:
-        stem_distribution = self.stem_decoder.run(in_sentence)
+        stem_distribution = self.stem_decoder.run(encoded_vector)
         certainties, idxs = stem_distribution.view(-1).topk(beam_width)
-        scores : List[float] = certainties
+        scores : torch.FloatTensor = certainties
         next_idxs = [idxs]
-        next_hidden = _inflate(in_sentence, beam_width)
-        back_pointers = []
+        next_hidden = _inflate(encoded_vector, beam_width)
+        back_pointers : List[torch.LongTensor] = []
 
         for i in range(max_args):
             next_arg_dist, next_hidden = \
@@ -96,12 +107,12 @@ class EncStructPredictor(TacticPredictor):
             beam_scores = next_arg_dist + scores.unsqueeze(1).expand_as(next_arg_dist)
             best_scores, best_beam_idxs = beam_scores.view(-1).topk(beam_width)
             scores = best_scores
-            back_pointer_row = best_beam_idxs / next_arg_dist.size(1)
-            back_pointers.append(back_pointer_row)
-            next_idxs.append(best_beam_idxs - back_pointer_row * next_arg_dist.size(1))
+            back_pointers_row = best_beam_idxs / next_arg_dist.size(1)
+            back_pointers.append(back_pointers_row)
+            next_idxs.append(best_beam_idxs - back_pointers_row * next_arg_dist.size(1))
             next_hidden = next_hidden.index_select(1,
                                                    cast(torch.LongTensor,
-                                                        back_pointers.squeeze()))
+                                                        back_pointers_row.squeeze()))
 
         results = []
         for i in range(beam_width):
@@ -120,15 +131,51 @@ class EncStructPredictor(TacticPredictor):
     def getOptions(self) -> List[Tuple[str, str]]:
         return self.options
 
-Checkpoint = Tuple[Dict[Any, Any], float]
+class RNNEncoder(nn.Module):
+    def __init__(self, input_vocab_size : int, hidden_size : int,
+                 num_layers : int, batch_size : int = 1) -> None:
+        super(RNNEncoder, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
 
-def train(dataset : Any,
-          input_vocab_size : int, output_vocab_size : int, hidden_size : int,
-          learning_rate : float, num_encoder_layers : int,
+        self.embedding = maybe_cuda(nn.Embedding(input_vocab_size, hidden_size))
+        self.gru = maybe_cuda(nn.GRU(hidden_size, hidden_size))
+        pass
+    pass
+class StemClassifier(nn.Module):
+    def __init__(self, hidden_size : int, output_vocab_size : int,
+                 num_layers : int, batch_size : int = 1) -> None:
+        super(StemClassifier, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+
+        self.layers = [maybe_cuda(nn.Linear(hidden_size, hidden_size))
+                       for _ in range(num_layers - 1)]
+        self.out_layer = maybe_cuda(nn.Linear(hidden_size, output_vocab_size))
+        self.softmax = maybe_cuda(nn.LogSoftmax(dim=1))
+    pass
+class ArgumentDecoder(nn.Module):
+    def __init__(self, hidden_size : int, num_layers : int, batch_size : int = 1)\
+        -> None:
+        super(ArgumentDecoder, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+    pass
+
+Checkpoint = Tuple[Dict[Any, Any], Dict[Any, Any], Dict[Any, Any], float]
+
+def train(dataset : StructDataset, input_vocab_size : int, output_vocab_size : int,
+          hidden_size : int, learning_rate : float,
+          num_encoder_layers : int, num_decoder_layers : int,
           max_length : int, num_epochs : int, batch_size : int,
           print_every : int, optimizer_f : Callable[..., Optimizer]) \
           -> Iterable[Checkpoint]:
-
+    print("Initializing PyTorch...")
+    hyps_stream = [[inputFromSentence(hyp, max_length) for hyp in hyps]
+                   for hyps, goal, struct in dataset]
     pass
 
 def decode_tactic_structure(term_tokenizer : Tokenizer, stem_embedding : SimpleEmbedding,
@@ -140,7 +187,8 @@ def decode_tactic_structure(term_tokenizer : Tokenizer, stem_embedding : SimpleE
             return serapi_instance.get_first_var_in_hyp(hyps[idx-1])
     stem_idx, arg_hyp_idxs = struct
     return " ".join([stem_embedding.decode_token(stem_idx)] +
-                    [get_var(hyp_idx) for hyp_idx in arg_hyp_idxs])
+                    [get_var(hyp_idx) for hyp_idx in takewhile(lambda idx: idx > 0,
+                                                               arg_hyp_idxs)])
 
 def encode_seq_structural_data(data : RawDataset,
                                context_tokenizer_type : \
@@ -173,7 +221,12 @@ def hyp_index(hyps : List[str], arg : str) -> int:
     return 0
 
 def main(arg_list : List[str]) -> None:
-    args = take_std_args(arg_list, "a structural pytorch model for proverbot")
+    argparser = argparse.ArgumentParser(description=
+                                        "a structural pytorch model for proverbot")
+    add_std_args(argparser)
+    argparser.add_argument("--num-decoder-layers", dest="num_decoder_layers",
+                           default=3, type=int)
+    args = argparser.parse_args(arg_list)
     print("Reading dataset...")
 
     raw_data = read_text_data(args.scrape_file)
@@ -192,12 +245,13 @@ def main(arg_list : List[str]) -> None:
     print("Encoded data in {:.2f}".format(timeTaken))
 
     checkpoints = train(dataset, tokenizer.numTokens(), embedding.num_tokens(),
-                        args.hidden_size,
-                        args.learning_rate, args.num_encoder_layers,
+                        args.hidden_size, args.learning_rate,
+                        args.num_encoder_layers, args.num_decoder_layers,
                         args.max_length, args.num_epochs, args.batch_size,
                         args.print_every, optimizers[args.optimizer])
 
-    for epoch, (encoder_state, training_loss) in enumerate(checkpoints):
+    for epoch, (encoder_state, stem_decoder_state, arg_decoder_state, training_loss) \
+        in enumerate(checkpoints):
         state = {'epoch':epoch,
                  'training-loss': training_loss,
                  'tokenizer':tokenizer,
@@ -205,8 +259,11 @@ def main(arg_list : List[str]) -> None:
                  'optimizer':args.optimizer,
                  'learning-rate':args.learning_rate,
                  'embedding': embedding,
-                 'neural-encoder':encoder_state,
+                 'encoder':encoder_state,
+                 'stem-decoder':stem_decoder_state,
+                 'arg-decoder':arg_decoder_state,
                  'num-encoder-layers':args.num_encoder_layers,
+                 'num-decoder-layers':args.num_decoder_layers,
                  'max-length': args.max_length,
                  'hidden-size' : args.hidden_size,
                  'num-keywords' : args.num_keywords,
