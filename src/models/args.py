a712702553843f8f80fd99cc85a99ef02a48de8a
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-12-02 17:58:09 -0800

    Smaller batch defaults

--- src/models/args.py
+++ src/models/args.py
@@ -15,11 +15,11 @@ optimizers = {
 def add_std_args(parser : argparse.ArgumentParser) -> None:
     parser.add_argument("scrape_file")
     parser.add_argument("save_file")
-    parser.add_argument("--num-epochs", dest="num_epochs", default=30, type=int)
-    parser.add_argument("--batch-size", dest="batch_size", default=4096, type=int)
+    parser.add_argument("--num-epochs", dest="num_epochs", default=15, type=int)
+    parser.add_argument("--batch-size", dest="batch_size", default=256, type=int)
     parser.add_argument("--max-length", dest="max_length", default=100, type=int)
     parser.add_argument("--max-tuples", dest="max_tuples", default=None, type=int)
-    parser.add_argument("--print-every", dest="print_every", default=10, type=int)
+    parser.add_argument("--print-every", dest="print_every", default=5, type=int)
     parser.add_argument("--hidden-size", dest="hidden_size", default=128, type=int)
     parser.add_argument("--learning-rate", dest="learning_rate",
                         default=.7, type=float)
