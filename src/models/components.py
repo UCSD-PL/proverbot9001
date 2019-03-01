e9bba678e5847935bb8edd1e29629125d5fb420a
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-02-28 16:45:17 -0800

    Fixes for generic dnn structure

--- src/models/components.py
+++ src/models/components.py
@@ -39,26 +39,28 @@ class SimpleEmbedding(Embedding):
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from util import *
 
 class DNNClassifier(nn.Module):
     def __init__(self, input_vocab_size : int, hidden_size : int, output_vocab_size : int,
-                 num_layers : int, batch_size : int=1) -> None:
+                 num_layers : int) -> None:
         super(DNNClassifier, self).__init__()
         self.num_layers = num_layers
-        self.batch_size = batch_size
         self.in_layer = maybe_cuda(nn.Linear(input_vocab_size, hidden_size))
-        self.layers = [maybe_cuda(nn.Linear(hidden_size, hidden_size))
-                       for _ in range(num_layers)]
+        for i in range(num_layers - 1):
+            self.add_module("_layer{}".format(i),
+                            maybe_cuda(nn.Linear(hidden_size, hidden_size)))
         self.out_layer = maybe_cuda(nn.Linear(hidden_size, output_vocab_size))
         self.softmax = maybe_cuda(nn.LogSoftmax(dim=1))
 
     def forward(self, input : torch.FloatTensor) -> torch.FloatTensor:
-        layer_values = self.in_layer(input)
-        for i in range(self.num_layers):
+        layer_values = self.in_layer(maybe_cuda(Variable(input)))
+        for i in range(self.num_layers - 1):
             layer_values = F.relu(layer_values)
-            layer_values = self.layers[i](layer_values)
-        return self.softmax(self.out_layer(layer_values))
+            layer_values = getattr(self, "_layer{}".format(i))(layer_values)
+        layer_values = F.relu(layer_values)
+        return self.softmax(self.out_layer(layer_values)).view(input.size()[0], -1)
 
 class EncoderDNN(nn.Module):
     def __init__(self, input_vocab_size : int, hidden_size : int, output_vocab_size : int,
