90740de882265d4f555719f55a8b37b4d86d23f9
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-03-07 11:41:24 -0800

    Add verbose arg to svm stubs

--- stubs/sklearn/svm.pyi
+++ stubs/sklearn/svm.pyi
@@ -3,7 +3,8 @@
 from typing import Any, List
 
 class SVC:
-    def __init__(self, gamma:Any='scale',kernel:str='rbf', probability: bool=False) \
+    def __init__(self, gamma:Any='scale',kernel:str='rbf', probability: bool=False,
+                 verbose:bool=False) \
         -> None:
         ...
     def fit(self, inputs : List[List[float]], outputs : List[int]) -> None:
