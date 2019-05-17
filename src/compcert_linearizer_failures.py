8a6f0ca0b39fab3c934ff1669b45a8a3341d97ad
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-05-17 16:30:01 -0700

    A few more known un-linearizable proofs

--- src/compcert_linearizer_failures.py
+++ src/compcert_linearizer_failures.py
@@ -1,6 +1,10 @@
 compcert_failures = (
-  [ ["./backend/Stackingproof.v", "Lemma set_location"], # fails because of linearizing "eapply Mem.load_store_similar_2; eauto."
-    ["./cfrontend/Cminorgenproof.v", "Lemma padding_freeable_invariant"],
-    ["./cfrontend/Cminorgenproof.v", "Lemma match_callstack_alloc_right"],
+  [
+      ["./driver/Complements.v", "Theorem transf_c_program_preservation"],
+      ["./driver/Complements.v", "Theorem transf_c_program_is_refinement"],
+      ["./backend/SelectDivProof.v", "Theorem eval_divuimm"],
+      ["./backend/Stackingproof.v", "Lemma set_location"], # fails because of linearizing "eapply Mem.load_store_similar_2; eauto."
+      ["./cfrontend/Cminorgenproof.v", "Lemma padding_freeable_invariant"],
+      ["./cfrontend/Cminorgenproof.v", "Lemma match_callstack_alloc_right"],
   ]
 )
