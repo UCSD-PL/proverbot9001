042940b26eb7a094c1289f6be216e15166b63f62
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-06-23 15:08:01 -0700

    Add some known linearizer skips

--- src/compcert_linearizer_failures.py
+++ src/compcert_linearizer_failures.py
@@ -6,5 +6,12 @@ compcert_failures = (
       ["./backend/Stackingproof.v", "Lemma set_location"], # fails because of linearizing "eapply Mem.load_store_similar_2; eauto."
       ["./cfrontend/Cminorgenproof.v", "Lemma padding_freeable_invariant"],
       ["./cfrontend/Cminorgenproof.v", "Lemma match_callstack_alloc_right"],
+      ["./backend/ValueDomain.v", "Lemma pincl_sound"],
+      ["./backend/Unusedglobproof.v", "Lemma add_ref_globvar_incl"],
+      ["./backend/Unusedglobproof.v", "Lemma initial_workset_incl"],
+      ["./backend/Unusedglobproof.v", "Theorem used_globals_sound"],
+      ["./backend/Unusedglobproof.v", "Theorem used_globals_incl"],
+
+      ["./lib/Maps.v", "Remark xelements_empty"],
   ]
 )
