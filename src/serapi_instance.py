b3d4b72ac070abb1859cfa8bbd6d6d8647f6a0d6
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-07-08 08:52:24 -0700

    Better lemma name parsing

--- src/serapi_instance.py
+++ src/serapi_instance.py
@@ -1107,7 +1107,7 @@ def progn(*args):
     return args[-1]
 
 def lemma_name_from_statement(stmt : str) -> str:
-    lemma_match = re.match("\s*\S+\s+(\w+)", stmt)
+    lemma_match = re.match("\s*\S+\s+([\w']+)", stmt)
     assert lemma_match, stmt
     lemma_name = lemma_match.group(1)
     assert ":" not in lemma_name, stmt
