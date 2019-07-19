609d56a0ee15bb4b1ae5ce2a9db7dda6a80b82fc
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-07-19 14:37:50 -0700

    Add type stubs for pathlib_revised

---
+++ stubs/other/pathlib_revised.pyi
@@ -0,0 +1,43 @@
+from typing import overload, IO
+class Path():
+    def __init__(self, str_path : str) -> None:
+        ...
+    def with_suffix(self, new_suffix : str) -> Path:
+        ...
+    @property
+    def stem(self) -> str:
+        ...
+    @property
+    def suffix(self) -> str:
+        ...
+    @property
+    def parent(self) -> Path:
+        pass
+    def open(self, mode='r',newline='\n') -> IO[str]:
+        ...
+    @overload
+    def __truediv__(self, other : Path) -> Path:
+        ...
+    @overload
+    def __truediv__(self, other : str) -> Path:
+        ...
+    pass
+class Path2(Path):
+    def __init__(self, str_path : str) -> None:
+        ...
+    @property
+    def parent(self) -> Path2:
+        pass
+    def with_suffix(self, new_suffix : str) -> Path2:
+        ...
+    def exists(self) -> bool:
+        ...
+    def copyfile(self, dest : Path) -> None:
+        ...
+    @overload
+    def __truediv__(self, other : Path) -> Path2:
+        ...
+    @overload
+    def __truediv__(self, other : str) -> Path2:
+        ...
+    pass
