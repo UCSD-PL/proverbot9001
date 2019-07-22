86dccfcbae85c0a3522255ca2472742d6d6a6a1c
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-07-22 10:53:56 -0700

    Fix weights string not getting formatted

--- src/models/tactic_predictor.py
+++ src/models/tactic_predictor.py
@@ -458,7 +458,7 @@ def save_checkpoints(predictor_name : str,
                      metadata : MetadataType, arg_values : Namespace,
                      checkpoints_stream : Iterable[StateType]):
     for epoch, predictor_state in enumerate(checkpoints_stream, start=1):
-        epoch_filename = Path2(str(arg_values.save_file.with_suffix("")) + "-{epoch}.dat")
+        epoch_filename = Path2(str(arg_values.save_file.with_suffix("")) + f"-{epoch}.dat")
         with epoch_filename.open(mode='wb') as f:
             print("=> Saving checkpoint at epoch {}".format(epoch))
             torch.save((predictor_name, (arg_values, metadata, predictor_state)), f)
