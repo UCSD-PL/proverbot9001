4f414bcd7c3ab5e0ba37c975c2d5025d1ae36b51
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-09-03 12:46:50 -0700

    Numerical verbose on search_file.py

--- src/search_file.py
+++ src/search_file.py
@@ -129,7 +129,7 @@ def parse_arguments(args_list : List[str]) -> Tuple[argparse.Namespace,
     parser.add_argument("--debug", "-vv", help="debug output",
                         action='store_true')
     parser.add_argument("--verbose", "-v", help="verbose output",
-                        action='store_true')
+                        action="count", default=0)
     parser.add_argument("--progress", "-P", help="show progress of files",
                         action='store_true')
     parser.add_argument("--hardfail", "-f", help="fail when hitting a coq anomaly",
