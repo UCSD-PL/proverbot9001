a9845da71719aea875dd0fdf4a0eb5679740d9a9
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2019-11-06 09:53:52 -0800

    Add a section stack for exhaustive end checking

--- src/search_file.py
+++ src/search_file.py
@@ -225,6 +225,7 @@ def search_file(args : argparse.Namespace, coqargs : List[str],
     num_commands_total = len(commands_in)
     lemma_statement = ""
     module_stack : List[str] = []
+    section_stack : List[str] = []
 
     # Run vernacular until the next proof (or end of file)
     def run_to_next_proof(coq : serapi_instance.SerapiInstance, pbar : tqdm) -> str:
@@ -232,6 +233,7 @@ def search_file(args : argparse.Namespace, coqargs : List[str],
         nonlocal commands_in
         nonlocal blocks_out
         nonlocal module_stack
+        nonlocal section_stack
         vernacs : List[str] = []
         assert not coq.proof_context
         starttime = time.time()
@@ -241,7 +243,7 @@ def search_file(args : argparse.Namespace, coqargs : List[str],
             coq.run_stmt(next_in_command, timeout=60)
             if not coq.proof_context:
                 vernacs.append(next_in_command)
-            update_module_stack(next_in_command, module_stack)
+            update_module_stack(next_in_command, module_stack, section_stack)
             pbar.update(1)
         append_time(args, "vernac", time.time() - starttime)
         if len(vernacs) > 0:
@@ -336,6 +338,7 @@ def search_file(args : argparse.Namespace, coqargs : List[str],
                                                          args.filename,
                                                          commands_in,
                                                          module_stack,
+                                                         section_stack,
                                                          bar_idx)
                            pbar.update(num_original_commands_run)
                         except FileNotFoundError:
@@ -621,6 +624,7 @@ def check_solution_vfile_args(args : argparse.Namespace, model_name : str,
 def replay_solution_vfile(args : argparse.Namespace, coq : serapi_instance.SerapiInstance,
                           model_name : str, filename : Path2, commands_in : List[str],
                           module_stack : List[str],
+                          section_stack : List[str],
                           bar_idx : int) \
                           -> Tuple[List[str], List[str], List[DocumentBlock],
                                    int, int, int, int]:
@@ -697,7 +701,7 @@ def replay_solution_vfile(args : argparse.Namespace, coq : serapi_instance.Serap
                                                                commands_in_iter)
                             skip_sync_next_lemma = True
                             continue
-                        update_module_stack(saved_command, module_stack)
+                        update_module_stack(saved_command, module_stack, section_stack)
                         def normalize_command(cmd : str) -> str:
                             return re.sub("\s*\(\*(?:INCOMPLETE|FAILURE)\*\)", "",
                                           re.sub("Let", "Hypothesis",
@@ -1140,18 +1144,25 @@ def completed_proof(coq : serapi_instance.SerapiInstance) -> bool:
     else:
         return False
 
-def update_module_stack(cmd : str, module_stack : List[str]) -> None:
+def update_module_stack(cmd : str,
+                        module_stack : List[str],
+                        section_stack : List[str]) -> None:
     stripped_cmd = serapi_instance.kill_comments(cmd).strip()
     module_start_match = re.match(r"Module (\w*)\b(?!.*:=)", stripped_cmd)
-    module_end_match = re.match(r"End (\w*)\.", stripped_cmd)
+    section_start_match = re.match(r"Section (\w*)\b(?!.*:=)", stripped_cmd)
+    end_match = re.match(r"End (\w*)\.", stripped_cmd)
     if module_start_match:
         module_stack.append(module_start_match.group(1))
-    elif module_end_match:
-        if module_stack:
-            if module_stack[-1] == module_end_match.group(1):
-                started_module_name = module_stack.pop()
-            else:
-                eprint(f"Unrecognized End \"{cmd}\"")
+    elif section_start_match:
+        section_stack.append(section_start_match.group(1))
+    elif end_match:
+        if module_stack and module_stack[-1] == end_match.group(1):
+            module_stack.pop()
+        elif section_stack and section_stack[-1] == end_match.group(1):
+            section_stack.pop()
+        else:
+            eprint(f"Unrecognized End \"{cmd}\"")
+            assert False
 
 if __name__ == "__main__":
     main(sys.argv[1:], 0)
