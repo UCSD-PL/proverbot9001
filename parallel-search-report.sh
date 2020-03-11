1be6ec1fb75829750c8b43b290278b35575c738d
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2020-03-11 13:57:11 -0700

    More drop-in compatible parallel_search_report.sh

--- parallel-search-report.sh
+++ parallel-search-report.sh
@@ -11,11 +11,34 @@ while [ -L "$src" ]; do
 done
 MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
 
+PARAMS=""
+OUTDIR=$MYDIR/search-report
 : "${NUM_THREADS:=5}"
+
+while (( "$#" )); do
+    case "$1" in
+        -o|--output)
+            OUTDIR=$2
+            shift 2
+            ;;
+        --output=*) # Not sure this is working yet
+            OUTDIR="${i#*=}"
+            shift
+            ;;
+        -j|--num-threads)
+            NUM_THREADS=$2
+            shift 2
+            ;;
+        *) # preserve all other arguments
+            PARAMS="$PARAMS $1"
+            shift
+            ;;
+    esac
+done
+
 : "${JOBS_FILE:=$MYDIR/data/compcert-test-files.txt}"
-: "${OUTDIR:=$MYDIR/search-report}"
 mkdir -p logs
 parallel --tmuxpane -j $NUM_THREADS -a $JOBS_FILE --fg \
          "tmux select-layout even-vertical && ulimit -s unlimited &&
-          python3.7 $MYDIR/src/search_file.py -o $OUTDIR $@ {} 2> logs/{/.}.txt --proof-times=logs/{/.}-times.txt"
-cat $JOBS_FILE | xargs python3.7 $MYDIR/src/search_report.py -o $OUTDIR $@
+          python3.7 $MYDIR/src/search_file.py -o $OUTDIR $PARAMS {} 2> logs/{/.}.txt --proof-times=logs/{/.}-times.txt"
+cat $JOBS_FILE | xargs python3.7 $MYDIR/src/search_report.py -o $OUTDIR $PARAMS
