6bc9d467adcbb080fcc78ee4e03ba28cd9e16337
Alex Sanchez-Stern <alexss@pop-os.localdomain>
2020-04-13 10:30:13 -0700

    Rust formatting

--- dataloader/src/models/goal_enc_evaluator.rs
+++ dataloader/src/models/goal_enc_evaluator.rs
@@ -1,16 +1,16 @@
+use bincode::{deserialize, serialize};
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use pyo3::ToPyObject;
-use std::fs::File;
 use rayon::prelude::*;
-use bincode::{deserialize, serialize};
+use std::fs::File;
 
 use crate::context_filter::filter_data_by_key;
-use crate::scraped_data::*;
-use crate::tokenizer::{Tokenizer, normalize_sentence_length};
 use crate::models::evaluator_common::*;
+use crate::scraped_data::*;
+use crate::tokenizer::{normalize_sentence_length, Tokenizer};
 
-#[pyclass(module="dataloader")]
+#[pyclass(module = "dataloader")]
 pub struct GoalEncMetadata {
     tokenizer: Tokenizer,
 }
@@ -37,14 +37,12 @@ pub fn goals_to_total_distances_tensors(
     metadata: Option<&GoalEncMetadata>,
 ) -> Result<(GoalEncMetadata, LongTensor2D, FloatTensor1D), String> {
     let raw_data =
-        scraped_from_file(File::open(filename).map_err(|_err| "Failed to open file")?)
-        .collect();
+        scraped_from_file(File::open(filename).map_err(|_err| "Failed to open file")?).collect();
     let distanced = tactic_distances(raw_data);
-    let filtered_data = filter_data_by_key(&args, &args.context_filter,
-                                           distanced,
-                                           |distanced| &(*distanced).0);
-    let (tactics, distances) : (Vec<ScrapedTactic>, Vec<usize>) =
-        filtered_data.into_iter().unzip();
+    let filtered_data = filter_data_by_key(&args, &args.context_filter, distanced, |distanced| {
+        &(*distanced).0
+    });
+    let (tactics, distances): (Vec<ScrapedTactic>, Vec<usize>) = filtered_data.into_iter().unzip();
     let tokenizer = match metadata {
         Some(meta) => meta.tokenizer.clone(),
         None => {
@@ -62,7 +60,8 @@ pub fn goals_to_total_distances_tensors(
 
     Ok((GoalEncMetadata{tokenizer},
         tokenized_goals,
-        normalize_distances(args.max_distance, distances)))
+        normalize_distances(args.max_distance, distances),
+    ))
 }
 
 pub fn goal_enc_get_num_tokens(metadata: &GoalEncMetadata) -> i64 {
