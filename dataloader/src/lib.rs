8b29dfe55604c1201308c8c83345d393014c5022
Alex Sanchez-Stern <alexss@pop-os.localdomain>
2020-04-14 13:41:02 -0700

    Add GPL header to a bunch of rust files

--- dataloader/src/lib.rs
+++ dataloader/src/lib.rs
@@ -1,4 +1,25 @@
 #![feature(vec_remove_item)]
+/* *********************************************************************** */
+//
+//    This file is part of Proverbot9001.
+//
+//    Proverbot9001 is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Proverbot9001 is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Proverbot9001.  If not, see <https://www.gnu.org/licenses/>.
+//
+//    Copyright 2019 Alex Sanchez-Stern and Yousef Alhessi
+//
+/* *********************************************************************** */
+
 use pyo3::exceptions;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
