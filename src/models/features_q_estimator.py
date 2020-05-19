1ffeb82f05038326d7353a908d3fa5d3a170a52f
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-05-19 10:52:11 -0700

    Linter fixes

--- src/models/features_q_estimator.py
+++ src/models/features_q_estimator.py
@@ -23,18 +23,19 @@ import serapi_instance
 import tokenizer
 
 from models.q_estimator import QEstimator
-from util import maybe_cuda, eprint
+from util import maybe_cuda
 from format import TacticContext
 from models.components import WordFeaturesEncoder, DNNScorer
 
 import torch
 import torch.nn as nn
-from torch import optim, autograd
+from torch import optim
 import argparse
 import sys
 from pathlib_revised import Path2
 
 from typing import Dict, List, Tuple, cast, BinaryIO, TypeVar
+
 
 class FeaturesQEstimator(QEstimator):
     def __init__(self, learning_rate: float) -> None:
@@ -42,9 +43,10 @@ class FeaturesQEstimator(QEstimator):
                                     2, 128, 3)
         self.optimizer = optim.SGD(self.model.parameters(), learning_rate)
         self.criterion = nn.MSELoss()
-        self.tactic_map : Dict[str, int] = {}
-        self.token_map : Dict[str, int] = {}
+        self.tactic_map: Dict[str, int] = {}
+        self.token_map: Dict[str, int] = {}
         pass
+
     def __call__(self, inputs: List[Tuple[TacticContext, str]]) -> List[float]:
         state_word_features_batch, vec_features_batch \
             = zip(*[self._features(state) for (state, action) in inputs])
@@ -57,32 +59,44 @@ class FeaturesQEstimator(QEstimator):
         output = self.model(torch.LongTensor(all_word_features_batch),
                             torch.FloatTensor(vec_features_batch))
         return list(output)
+
     def train(self, samples: List[Tuple[TacticContext, str, float]]) -> None:
         self.optimizer.zero_grad()
-        state_word_features, vec_features = zip(*[self._features(state) for state, _, _ in samples])
-        encoded_actions = [self._encode_action(state, action) for state, action, _ in samples]
-        all_word_features = [list(ea) + swf for ea, swf in zip(encoded_actions, state_word_features)]
+        state_word_features, vec_features = zip(*[self._features(state)
+                                                  for state, _, _ in samples])
+        encoded_actions = [self._encode_action(state, action)
+                           for state, action, _ in samples]
+        all_word_features = [list(ea) + swf for ea, swf in
+                             zip(encoded_actions, state_word_features)]
         # with autograd.detect_anomaly():
         outputs = self.model(torch.LongTensor(all_word_features),
                              torch.FloatTensor(vec_features))
-        expected_outputs = maybe_cuda(torch.FloatTensor([output for _, _, output in samples]))
+        expected_outputs = maybe_cuda(torch.FloatTensor(
+            [output for _, _, output in samples]))
         loss = self.criterion(outputs, expected_outputs)
         loss.backward()
         self.optimizer.step()
-    def _features(self, context: TacticContext) -> Tuple[List[int], List[float]]:
+
+    def _features(self, context: TacticContext) \
+            -> Tuple[List[int], List[float]]:
         if len(context.prev_tactics) > 0:
             prev_tactic = serapi_instance.get_stem(context.prev_tactics[-1])
             prev_tactic_index = emap_lookup(self.tactic_map, 32, prev_tactic)
         else:
             prev_tactic_index = 0
         if context.goal != "":
-            goal_head_index = emap_lookup(self.token_map, 128, tokenizer.get_words(context.goal)[0])
+            goal_head_index = emap_lookup(self.token_map, 128,
+                                          tokenizer.get_words(context.goal)[0])
         else:
             goal_head_index = 0
-        goal_length_feature = min(len(tokenizer.get_words(context.goal)), 100) / 100
+        goal_length_feature = min(len(tokenizer.get_words(context.goal)),
+                                  100) / 100
         num_hyps_feature = min(len(context.hypotheses), 30) / 30
-        return [prev_tactic_index, goal_head_index], [goal_length_feature, num_hyps_feature]
-    def _encode_action(self, context: TacticContext, action: str) -> Tuple[int, int]:
+        return [prev_tactic_index, goal_head_index], \
+            [goal_length_feature, num_hyps_feature]
+
+    def _encode_action(self, context: TacticContext, action: str) \
+            -> Tuple[int, int]:
         stem, argument = serapi_instance.split_tactic(action)
         stem_idx = emap_lookup(self.tactic_map, 32, stem)
         all_premises = context.hypotheses + context.relevant_lemmas
@@ -90,23 +104,31 @@ class FeaturesQEstimator(QEstimator):
         if stripped_arg == "":
             arg_idx = 0
         else:
-            index_hyp_vars = dict(serapi_instance.get_indexed_vars_in_hyps(all_premises))
+            index_hyp_vars = dict(serapi_instance.get_indexed_vars_in_hyps(
+                all_premises))
             if stripped_arg in index_hyp_vars:
-                hyp_varw, _, rest = all_premises[index_hyp_vars[stripped_arg]].partition(":")
-                arg_idx = emap_lookup(self.token_map, 128, tokenizer.get_words(rest)[0]) + 2
+                hyp_varw, _, rest = all_premises[index_hyp_vars[stripped_arg]]\
+                    .partition(":")
+                arg_idx = emap_lookup(self.token_map, 128,
+                                      tokenizer.get_words(rest)[0]) + 2
             else:
                 goal_symbols = tokenizer.get_symbols(context.goal)
                 if stripped_arg in goal_symbols:
-                    arg_idx = emap_lookup(self.token_map, 128, stripped_arg) + 128 + 2
+                    arg_idx = emap_lookup(self.token_map, 128,
+                                          stripped_arg) + 128 + 2
                 else:
                     arg_idx = 1
         return stem_idx, arg_idx
-    def save_weights(self, filename : Path2, args : argparse.Namespace) -> None:
+
+    def save_weights(self, filename: Path2, args: argparse.Namespace) -> None:
         with cast(BinaryIO, filename.open('wb')) as f:
-            torch.save(("features evaluator", args, sys.argv, self.model.state_dict()),
+            torch.save(("features evaluator", args, sys.argv,
+                        self.model.state_dict()),
                        f)
 
+
 T = TypeVar('T')
+
 
 def emap_lookup(emap: Dict[T, int], size: int, item: T):
     if item in emap:
@@ -117,15 +139,17 @@ def emap_lookup(emap: Dict[T, int], size: int, item: T):
     else:
         return 0
 
+
 class FeaturesQModel(nn.Module):
     def __init__(self,
-                 num_tactics : int,
-                 num_tokens : int,
-                 vec_features_size : int,
-                 hidden_size : int,
-                 num_layers : int) -> None:
+                 num_tactics: int,
+                 num_tokens: int,
+                 vec_features_size: int,
+                 hidden_size: int,
+                 num_layers: int) -> None:
         super().__init__()
-        # Consider making the word embedding the same for all token-type inputs, also for tactic-type inputs
+        # Consider making the word embedding the same for all
+        # token-type inputs, also for tactic-type inputs
         self._word_features_encoder = maybe_cuda(
             WordFeaturesEncoder([num_tactics, num_tokens * 2 + 2,
                                  num_tactics, num_tokens],
@@ -133,12 +157,14 @@ class FeaturesQModel(nn.Module):
         self._features_classifier = maybe_cuda(
             DNNScorer(hidden_size + vec_features_size,
                       hidden_size, num_layers))
+
     def forward(self,
-                word_features_batch : torch.LongTensor,
-                vec_features_batch : torch.FloatTensor) -> torch.FloatTensor:
+                word_features_batch: torch.LongTensor,
+                vec_features_batch: torch.FloatTensor) -> torch.FloatTensor:
         encoded_word_features = self._word_features_encoder(
             maybe_cuda(word_features_batch))
         scores = self._features_classifier(
-            torch.cat((encoded_word_features, maybe_cuda(vec_features_batch)), dim=1))\
-        .view(vec_features_batch.size()[0])
+            torch.cat((encoded_word_features, maybe_cuda(vec_features_batch)),
+                      dim=1))\
+                     .view(vec_features_batch.size()[0])
         return scores
