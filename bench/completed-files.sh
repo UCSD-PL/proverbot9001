987c13f1e35fb3b2ee638f5712513a03b4be21bc
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2020-06-17 12:35:14 -0700

    Fixes for completed files script

--- bench/completed-files.sh
+++ bench/completed-files.sh
@@ -1,1 +1,2 @@
-ls $1/*.html | sed "s/$1\///" | sed 's/Zs/\//g' | sed 's/Zdv.html/.v/' | sed 's/ZZ/Z/g'
+[ "$#" -eq 1 ] || (echo "1 argument required, $# provided" && exit 1)
+ls $1/*.html | grep -v "report.html" | sed "s~$1/~~" | sed 's/Zs/\//g' | sed 's/Zdv.html/.v/' | sed 's/ZZ/Z/g'
