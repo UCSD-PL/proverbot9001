3663e602569e19f2ce00ffe8da6bd8821e3a8115
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-09-06 17:15:33 -0700

    Style fixes

--- get_column.py
+++ get_column.py
@@ -4,10 +4,10 @@ import csv
 import sys
 import argparse
 
+
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description=
-        "Like cut, but respects quotes")
+        description="Like cut, but respects quotes")
     parser.add_argument("-f", "--field", default=0, type=int)
     parser.add_argument("-d", "--delimiter", default=",", type=str)
     args = parser.parse_args()
@@ -15,6 +15,7 @@ def main() -> None:
     reader = csv.reader(sys.stdin, delimiter=args.delimiter)
     for row in reader:
         print(row[args.field])
+
 
 if __name__ == "__main__":
     main()
