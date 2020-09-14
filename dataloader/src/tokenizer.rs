059dfa748cdf9f14b9f59d8b6259478dea84f92c
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-09-14 16:31:17 -0700

    Recognize more coq syntax in tokenizer

--- dataloader/src/tokenizer.rs
+++ dataloader/src/tokenizer.rs
@@ -160,7 +160,8 @@ pub fn get_words(string: &str) -> Vec<&str> {
 }
 pub fn get_symbols(string: &str) -> Vec<&str> {
     lazy_static! {
-        static ref WORDS: Regex = Regex::new(r"(,|:=|:|=>|\)|\()|(([[:word:]\.]|')+)").unwrap();
+        static ref WORDS: Regex =
+            Regex::new(r"(,|:=|:|=>|=|->|@@|\{|\}|/\\|\)|\(|\|\}|\{\||;|\|)|\.|(([[:word:]'])+)").unwrap();
     }
     WORDS.find_iter(string).map(|m| m.as_str()).collect()
 }
