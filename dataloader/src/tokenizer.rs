4492a501cacff90755dc40d6ace804b2aef026e4
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-09-14 21:43:30 -0700

    Tokenizer fixes

--- dataloader/src/tokenizer.rs
+++ dataloader/src/tokenizer.rs
@@ -167,14 +167,17 @@ impl Tokenizer {
 }
 pub fn get_words(string: &str) -> Vec<&str> {
     lazy_static! {
-        static ref WORDS: Regex = Regex::new(r"(,|\.+|:\b|:=|=>|\)|\()|(([[:word:]]|')+)").unwrap();
+        static ref WORDS: Regex =
+            Regex::new(r"(,|:=|:|=>|=|->|@@|\{|\}|/\\|\)|\(|\|\}|\{\||;|\|)|(([[:word:]'\.])+)")
+                .unwrap();
     }
     WORDS.find_iter(string).map(|m| m.as_str()).collect()
 }
 pub fn get_symbols(string: &str) -> Vec<&str> {
     lazy_static! {
         static ref WORDS: Regex =
-            Regex::new(r"(,|:=|:|=>|=|->|@@|\{|\}|/\\|\)|\(|\|\}|\{\||;|\|)|\.|(([[:word:]'])+)").unwrap();
+            Regex::new(r"(,|:=|:|=>|=|->|@@|\{|\}|/\\|\)|\(|\|\}|\{\||;|\|)|\.|(([[:word:]'])+)")
+                .unwrap();
     }
     WORDS.find_iter(string).map(|m| m.as_str()).collect()
 }
