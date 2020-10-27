6ad9743b194262994cf99600c3f3ee67589399c2
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2020-10-27 15:20:17 -0700

    Scrape all files, not just training ones
    The new search report wants a scrape of the test files tooc

--- bench/scrape_bench.sh
+++ bench/scrape_bench.sh
@@ -13,11 +13,11 @@ MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
 
 [ "$#" -ge 1 ] || (echo "at least 1 argument required, $# provided" && exit 1)
 PROJECT=$1
-TRAIN_FILES=$MYDIR/$PROJECT/train-files.txt
-[[ -f $TRAIN_FILES ]] || (echo "Cannot find training file list at $TRAIN_FILES" && exit 1)
+SCRAPE_FILES=$MYDIR/$PROJECT/files.txt
+[[ -f $SCRAPE_FILES ]] || (echo "Cannot find training file list at $SCRAPE_FILES" && exit 1)
 shift
 
 cd $MYDIR/$PROJECT
 
-cat "$TRAIN_FILES" | xargs python3 $MYDIR/../src/scrape.py -c -j3 -o /dev/null $@
+cat "$SCRAPE_FILES" | xargs python3 $MYDIR/../src/scrape.py -c -j3 -o /dev/null $@
 find -name "*.scrape" | xargs cat > $MYDIR/$PROJECT/scrape.txt
