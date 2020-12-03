92c4ef1cd2cb002d3739be1254c07c91a0e5cb54
Ubuntu <ubuntu@ip-172-31-7-52.us-east-2.compute.internal>
2020-12-03 19:03:16 +0000

    Fall back on files.txt if test-files.txt isn't there

--- bench/report_bench.sh
+++ bench/report_bench.sh
@@ -14,10 +14,12 @@ MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
 [ "$#" -ge 2 ] || (echo "at least 2 argument required, $# provided" && exit 1)
 PROJECT=$1
 WEIGHTS_ID=$2
-TEST_FILES=$MYDIR/$PROJECT/test-files.txt
-[[ -f $TEST_FILES ]] || (echo "Cannot find test file list at $TEST_FILES" && exit 1)
+TEST_FILES_FILE=test-files.txt
+[[ -f $MYDIR/$PROJECT/$TEST_FILES_FILE ]] || TEST_FILES_FILE=files.txt
+TEST_FILES=$MYDIR/$PROJECT/$TEST_FILES_FILE
+[[ -f $TEST_FILES ]] || (echo "Cannot find test file list" && exit 1)
 shift 2
 
 cd $MYDIR/$PROJECT
 
-cat test-files.txt | xargs $MYDIR/../src/search_file.py -o search-report-$WEIGHTS_ID -P --weightsfile=$MYDIR/../data/polyarg-weights-$WEIGHTS_ID.dat $@
+cat $TEST_FILES_FILE | xargs $MYDIR/../src/search_file.py -o search-report-$WEIGHTS_ID -P --weightsfile=$MYDIR/../data/polyarg-weights-$WEIGHTS_ID.dat $@
