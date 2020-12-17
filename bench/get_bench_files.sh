c1e00518b0bd66375588b33219ce70fb57e30b79
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-12-17 10:43:14 -0800

    Don't include bench files imported as .vo

--- bench/get_bench_files.sh
+++ bench/get_bench_files.sh
@@ -4,4 +4,4 @@ set -e
 
 [ "$#" -ne 1 ] || cd $1
 
-find -name "*.vo" -not -path "./_opam/*" | sed 's/.vo/.v/' > files.txt
+comm -12 <(find -name "*.vo" -not -path "./_opam/*" | sed 's/.vo/.v/' | sort) <(find -name "*.v" -not -path "./_opam/*" | sort) > files.txt
