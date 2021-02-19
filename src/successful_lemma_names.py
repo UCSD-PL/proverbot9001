8c7c132cbb2ad991ffa3be5916ac0f43dec84806
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-02-19 09:47:30 -0800

    Add script to find all successful lemma names in proof files

---
+++ src/successful_lemma_names.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sys
+from pathlib_revised import Path2
+
+import serapi_instance
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=
+        "Script which prints out the names of "
+        "successful lemmas in a search report")
+    parser.add_argument("proofs_files",
+                        help="-proofs.txt files",
+                        nargs="+",
+                        type=Path2)
+    args = parser.parse_args()
+
+    for filename in args.proofs_files:
+        with filename.open('r') as proof_file:
+            for line in proof_file:
+                (filename, module, lemma_stmt), sol = json.loads(line)
+                if sol["status"] == "SUCCESS":
+                    print(serapi_instance.lemma_name_from_statement(
+                        lemma_stmt))
+
+if __name__ == "__main__":
+    main()
