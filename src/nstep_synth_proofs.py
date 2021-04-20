a99db9aff16e94cc9fd9867263c6ef85663b354f
Yousef Alhessi <yalhessi@gmail.com>
2021-04-20 11:09:36 -0700

    WIP: initial work on writing synthetic proofs to file

--- src/nstep_synth_proofs.py
+++ src/nstep_synth_proofs.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import List, Tuple
+from typing import List, TextIO, Tuple
 
 from pathlib_revised import Path2
 
@@ -7,16 +7,38 @@ import coq_serapy
 import util
 
 
+def generate_synthetic_lemmas(coq: coq_serapy.SerapiInstance, 
+                            proof_commands: List[str], f: TextIO):
+    for cmd_idx in range(len(proof_commands)):
+        before_state, after_state = coq.tactic_context([]), coq.tactic_context([])
+        # NOTE: for now we're generating synth lemmas on anything that doesn't manipulate the goal
+        # for now only " h1 => g1 => g2 "
+
+        pass
+
 def generate_synthetic_file(args: argparse.Namespace,
                             filename: Path2,
                             proof_jobs: List[str]):
+    synth_filename = filename.get_stem().with_suffix('-synthetic.v')
+    with synth_filename.open('a') as synth_f: 
+        pass
+
+    proof_commands = coq_serapy.load_commands(filename)
     with coq_serapy.SerapiContext(["sertop", "--implicit",
                                    f"--topfile={filename}"],
                                   None,
                                   str(args.prelude)) as coq:
-        pass
-
-    pass
+        rest_commands = proof_commands
+        while True:
+            rest_commands, run_commands = coq.run_into_next_proof(rest_commands)
+            with synth_filename.open('a') as synth_f:
+                for cmd in run_commands[:-1]:  # discard starting the proof
+                    print(cmd, file=synth_f)
+                if coq_serapy.lemma_name_from_statement(run_commands[-1]) in proof_jobs:
+                    generate_synthetic_lemmas(coq, rest_commands, synth_f)
+                rest_commands, run_commands = coq.finish_proof(rest_commands)
+                for cmd in run_commands:
+                    print(cmd, file=synth_f)
 
 
 def main():
@@ -61,11 +83,11 @@ def get_proofs(args: argparse.Namespace,
                t: Tuple[int, str]) -> List[Tuple[str, str, str]]:
     idx, filename = t
     with util.silent():
-        cmds = coq_serapy.load_commands_preserve(
+        commands = coq_serapy.load_commands_preserve(
             args, idx, args.prelude / filename)
     return [(filename, module, cmd) for module, cmd in
             coq_serapy.lemmas_in_file(
-                filename, cmds, args.include_proof_relevant)]
+                filename, commands, args.include_proof_relevant)]
 
 
 if __name__ == "__main__":
