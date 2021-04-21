e3fabad6ac0281b4d9342d7daa8144ea1a6de13a
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-04-21 14:43:18 -0700

    Rough draft of synthetic proofs

--- src/nstep_synth_proofs.py
+++ src/nstep_synth_proofs.py
@@ -1,5 +1,6 @@
 import argparse
-from typing import List, TextIO, Tuple
+import re
+from typing import List, IO, Tuple
 
 from pathlib_revised import Path2
 
@@ -7,38 +8,116 @@ import coq_serapy
 import util
 
 
-def generate_synthetic_lemmas(coq: coq_serapy.SerapiInstance, 
-                            proof_commands: List[str], f: TextIO):
+def generate_synthetic_lemmas(coq: coq_serapy.SerapiInstance,
+                              lemma_stmt: str,
+                              proof_commands: List[str], f: IO[str]):
+    def write(s: str) -> None:
+        print(s, file=f)
+    break_after = False
     for cmd_idx in range(len(proof_commands)):
-        before_state, after_state = coq.tactic_context([]), coq.tactic_context([])
-        # NOTE: for now we're generating synth lemmas on anything that doesn't manipulate the goal
-        # for now only " h1 => g1 => g2 "
+        if proof_commands[cmd_idx].startswith("intro"):
+            continue
+        assert coq.proof_context
+        is_goal_open = re.match(r"\s*(?:\d+\s*:)?\s*[{]\s*",
+                                proof_commands[cmd_idx])
+        is_goal_close = re.match(r"\s*[}]\s*",
+                                 proof_commands[cmd_idx])
+        if coq.count_fg_goals() > 1 and not is_goal_open:
+            coq.run_stmt("{")
+        before_state = coq.tactic_context([])
+        coq.run_stmt(proof_commands[cmd_idx])
+        if is_goal_open or is_goal_close:
+            continue
+        print(coq.proof_context)
+        if not coq.proof_context or len(coq.proof_context.all_goals) == 0:
+            after_goals = []
+            break_after = True
+        else:
+            after_goals = coq.proof_context.fg_goals
+        # NOTE: for now we're generating synth lemmas on anything that doesn't
+        # manipulate the goal
 
-        pass
+        # for now only " h1 => g2 => g1 "
+
+        before_hyps = before_state.hypotheses
+
+        # This block ensures that if the induction tactic generalizes a
+        # variable already in context, we'll put that variable generalized
+        # in the subgoal hyp precedents.
+
+        generalized_vars = []
+        induction_match = re.match(r"\s*induction\s+(?P<var>\S)\.",
+                                   proof_commands[cmd_idx])
+        if induction_match:
+            var = induction_match.group('var')
+            for hyp in before_hyps:
+                hyp_vars = [h.strip() for h in
+                            coq_serapy.get_var_term_in_hyp(hyp).split(",")]
+                if var in hyp_vars:
+                    generalized_vars.append(
+                        var + " : " +
+                        coq_serapy.get_hyp_type(hyp))
+                    break
+
+        sec_name = "test_sec"
+        write(f"Section {sec_name}.")
+        for h in reversed(before_state.hypotheses):
+            write(f"  Hypothesis {h}.")
+        for gidx, goal in enumerate(after_goals):
+            gname = f"test_goal{gidx}"
+
+            new_hyps = generalized_vars + \
+                list(reversed(list(set(goal.hypotheses) -
+                                   set(before_hyps))))
+            gbody = ""
+
+            for new_hyp in new_hyps:
+                gbody += f"forall ({new_hyp}), "
+            gbody += goal.goal
+
+            write(f"  Hypothesis {gname}: {gbody}.")
+
+        synth_lemma_name = f"synth_lemma_{cmd_idx}"
+        write(f"  Lemma {synth_lemma_name}: {before_state.goal}.")
+        write(f"End {sec_name}.")
+        if break_after:
+            break
+
+    lemma_name = coq_serapy.lemma_name_from_statement(lemma_stmt)
+    coq.run_stmt(f"Reset {lemma_name}.")
+    coq.run_stmt(lemma_stmt)
+
 
 def generate_synthetic_file(args: argparse.Namespace,
                             filename: Path2,
                             proof_jobs: List[str]):
-    synth_filename = filename.get_stem().with_suffix('-synthetic.v')
-    with synth_filename.open('a') as synth_f: 
+    synth_filename = Path2(str(filename.with_suffix("")) + '-synthetic.v')
+    with synth_filename.open('w') as synth_f:
         pass
 
-    proof_commands = coq_serapy.load_commands(filename)
-    with coq_serapy.SerapiContext(["sertop", "--implicit",
-                                   f"--topfile={filename}"],
+    proof_commands = coq_serapy.load_commands(str(filename))
+    with coq_serapy.SerapiContext(["sertop", "--implicit"],
                                   None,
                                   str(args.prelude)) as coq:
+        coq.verbose = args.verbose
         rest_commands = proof_commands
         while True:
-            rest_commands, run_commands = coq.run_into_next_proof(rest_commands)
+            rest_commands, run_commands = coq.run_into_next_proof(
+                rest_commands)
             with synth_filename.open('a') as synth_f:
                 for cmd in run_commands[:-1]:  # discard starting the proof
-                    print(cmd, file=synth_f)
-                if coq_serapy.lemma_name_from_statement(run_commands[-1]) in proof_jobs:
-                    generate_synthetic_lemmas(coq, rest_commands, synth_f)
+                    print(cmd, file=synth_f, end="")
+                if not coq.proof_context:
+                    break
+                lemma_statement = run_commands[-1]
+                if coq_serapy.lemma_name_from_statement(
+                        run_commands[-1]) in proof_jobs:
+                    generate_synthetic_lemmas(coq, lemma_statement,
+                                              rest_commands, synth_f)
                 rest_commands, run_commands = coq.finish_proof(rest_commands)
+                print(lemma_statement, file=synth_f, end="")
                 for cmd in run_commands:
-                    print(cmd, file=synth_f)
+                    print(cmd, file=synth_f, end="")
 
 
 def main():
@@ -58,6 +137,7 @@ def main():
     parser.add_argument(
         "--context-filter", dest="context_filter", type=str,
         default="(goal-args+hyp-args+rel-lemma-args)%maxargs:1%default")
+    parser.add_argument("--verbose", "-v", action='count', default=0)
 
     args = parser.parse_args()
 
