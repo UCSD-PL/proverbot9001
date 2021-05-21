d4e921ff8b489db135491300b8db3f299d03e7e6
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-05-21 13:25:52 -0700

    Make get_bench_files script build first

--- bench/get_bench_files.sh
+++ bench/get_bench_files.sh
@@ -4,4 +4,10 @@ set -e
 
 [ "$#" -ne 1 ] || cd $1
 
+
+if [[ -f configure ]]; then
+    ./configure
+fi
+make -j
+
 comm -12 <(find -name "*.vo" -not -path "./_opam/*" | sed 's/.vo/.v/' | sort) <(find -name "*.v" -not -path "./_opam/*" | sort) > files.txt
