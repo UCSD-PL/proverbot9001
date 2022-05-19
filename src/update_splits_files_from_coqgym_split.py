6b6134e7ba1a3ab7d98261adf06199acb995958f
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2022-05-19 19:11:35 -0400

    Commit script for updating file lists

---
+++ src/update_splits_files_from_coqgym_split.py
@@ -0,0 +1,42 @@
+
+import argparse
+import sys
+import os.path
+import json
+from pathlib import Path
+from glob import glob
+
+from typing import List
+
+def main(arg_list: List[str]) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root", type=Path)
+    parser.add_argument("splits_file", type=Path)
+    parser.add_argument("coqgym_split_file", type=Path)
+    args = parser.parse_args()
+
+    with args.splits_file.open('r') as splits_f:
+        project_dicts = json.loads(splits_f.read())
+
+    with args.coqgym_split_file.open('r') as csplit_f:
+        project_split = json.loads(csplit_f.read())
+
+    for project_dict in project_dicts:
+        files = [str(Path(filename).with_suffix(".v")) for filename in
+                 glob(str(args.root / project_dict["project_name"] / "**/*.vo"),
+                      recursive=True)]
+        if len(files) == 0:
+            print(f"Warning: Project {project_dict['project_name']} has no files",
+                  file=sys.stderr)
+        if project_dict["project_name"] in (project_split["projs_train"]
+                                            + project_split["projs_valid"]):
+            project_dict["train_files"] = files
+        else:
+            assert project_dict["project_name"] in project_split["projs_test"], \
+              f"Can't find project {project_dict['project_name']} in coqgym split!"
+            project_dict["test_files"] = files
+
+    print(json.dumps(project_dicts, indent=4))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
