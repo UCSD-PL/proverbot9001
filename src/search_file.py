6e34c9ce7998ca9e421f1bfe43a329a77385b93d
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-08-24 20:01:22 -0400

    Write out time on success

--- src/search_file.py
+++ src/search_file.py
@@ -404,6 +404,7 @@ def search_file_multithreaded(args: argparse.Namespace) -> None:
         predictor = get_predictor(args)
         search_report.generate_report(args, predictor, project_dicts_from_args(args),
                                       time_taken)
+    write_time(args)
 
 def write_time(args: argparse.Namespace) -> None:
     global start_time
