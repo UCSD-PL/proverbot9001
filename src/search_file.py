b3448b1f30a5665fde6f925db75ed530b2136f27
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-09-12 17:29:18 -0400

    Write time at the end even when workers finish successfully

--- src/search_file.py
+++ src/search_file.py
@@ -375,7 +375,7 @@ def search_file_multithreaded(args: argparse.Namespace,
             worker.start()
         num_already_done = len(solved_jobs)
         os.makedirs(args.output_dir, exist_ok=True)
-        with util.sighandler_context(signal.SIGINT, functools.partial(write_time, args)):
+        with util.sighandler_context(signal.SIGINT, functools.partial(exit_early, args)):
             with tqdm(total=len(todo_jobs) + num_already_done,
                       dynamic_ncols=True, desc="Searching proofs") as bar:
                 bar.update(n=num_already_done)
@@ -403,6 +403,7 @@ def search_file_multithreaded(args: argparse.Namespace,
 
             for worker in workers:
                 worker.join()
+        write_time(args)
     time_taken = datetime.now() - start_time
 <<<<<<< HEAD
     write_time(args)
@@ -412,10 +413,14 @@ def search_file_multithreaded(args: argparse.Namespace,
         search_report.generate_report(args, predictor, project_dicts_from_args(args),
                                       time_taken)
 
-def write_time(args: argparse.Namespace, *rest_args) -> None:
+def write_time(args: argparse.Namespace) -> None:
+    global start_time
     with open(args.output_dir / "time_so_far.txt", 'w') as f:
         time_taken = datetime.now() - start_time
         print(str(time_taken), file=f)
+
+def exit_early(args: argparse.Namespace, *rest) -> None:
+    write_time(args)
     sys.exit()
 
 if __name__ == "__main__":
