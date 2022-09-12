5c176301b20d61d238070b19640d5d1bb3e693ea
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-09-12 17:28:38 -0400

    Fix some time calculations

--- src/search_file.py
+++ src/search_file.py
@@ -56,6 +56,7 @@ from tqdm import tqdm
 from pathlib import Path
 import torch
 
+start_time = datetime.now()
 
 def main(arg_list: List[str]) -> None:
     multiprocessing.set_start_method('spawn')
@@ -324,14 +325,15 @@ def remove_already_done_jobs(args: argparse.Namespace) -> None:
 
 def search_file_multithreaded(args: argparse.Namespace,
                               predictor: TacticPredictor) -> None:
+    global start_time
     start_time = datetime.now()
     if args.resume:
         solved_jobs = get_already_done_jobs(args)
         try:
             with open(args.output_dir / "time_so_far.txt", 'r') as f:
                 t = datetime.strptime(f.read(), "%H:%M:%S")
-                start_time = datetime.now() - timedelta(hours=t.hours,minutes=t.minutes,
-                                                        seconds=t.seconds)
+                start_time = datetime.now() - timedelta(hours=t.hour,minutes=t.minute,
+                                                        seconds=t.second)
         except FileNotFoundError:
             assert len(solved_jobs) == 0, "Trying to resume but can't find a time record!"
             pass
@@ -412,14 +414,14 @@ def search_file_multithreaded(args: argparse.Namespace,
 
             for worker in workers:
                 worker.join()
-    time_taken = start_time - datetime.now()
+    time_taken = datetime.now() - start_time
     if args.generate_report:
         search_report.generate_report(args, predictor, project_dicts_from_args(args),
                                       timedelta())
 
 def write_time(args: argparse.Namespace, *rest_args) -> None:
     with open(args.output_dir / "time_so_far.txt", 'w') as f:
-        time_taken = start_time - datetime.now()
+        time_taken = datetime.now() - start_time
         print(str(time_taken), file=f)
     sys.exit()
 
