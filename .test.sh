98dd7241f640fbbfca654662f7128b444a98d0d7
tpr <tom.p.reichel@gmail.com>
2022-09-23 17:14:06 -0500

    more adventurous test script + python3.7 isn't in the repos anymore

--- .test.sh
+++ .test.sh
@@ -13,3 +13,13 @@ export PATH=$PATH:~/.cargo/bin
 # run setup
 
 make setup
+
+# okay, we've got an install.
+
+make data/compcert-scrape.txt -j `nproc`
+
+python3 ./src/proverbot9001.py tokens data/compcert-scrape.txt tokens.txt
+
+python3 ./src/proverbot9001.py tactics data/compcert-scrape.txt tactics.txt
+
+make compcert-train
