93e5ae34519e1aedd53fd9a743fafffa9a174b63
Arpan Agrawal <arpan2@illinois.edu>
2022-11-14 14:20:42 -0500

    Adding 4 pre-computed examples, rev_rev fails, the others succeed.

---
+++ static/rev_rev_tree.js
@@ -0,0 +1,186 @@
+var treeData = {'name': 'rev_rev', 'children': [{'name': 'eauto.'}, {'name': 'unfold rev.', 'children': [{'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'eauto.'}]}]}, {'name': 'eauto.'}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.', 'children': [{'name': 'unfold rev.'}, {'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.'}, {'name': 'idtac.'}]}, {'name': 'eauto.'}]}]}]}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'eauto.'}]}]}]}, {'name': 'eauto.'}]}]}]}, {'name': 'eauto.'}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.', 'children': [{'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'case l.'}]}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}, {'name': 'simpl.'}]}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.'}, {'name': 'idtac.'}]}, {'name': 'eauto.'}]}]}]}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.', 'children': [{'name': 'eauto.'}, {'name': 'intuition.'}, {'name': 'try easy.'}, {'name': 'unfold rev.', 'children': [{'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.'}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.'}, {'name': 'eauto.'}]}]}]}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'eauto.'}]}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.', 'children': [{'name': 'unfold app.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}, {'name': 'simpl.'}]}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.'}, {'name': 'idtac.'}]}, {'name': 'eauto.'}]}]}]}, {'name': 'f_equal.'}, {'name': 'try apply l.'}, {'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'unfold rev.', 'children': [{'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'case l.'}]}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.'}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.'}, {'name': 'eauto.'}]}]}]}, {'name': 'eauto.'}, {'name': 'unfold app.'}, {'name': 'case l.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.', 'children': [{'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}, {'name': 'unfold app.'}]}, {'name': 'simpl.', 'children': [{'name': 'intros.'}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.'}, {'name': 'try congruence.'}]}, {'name': 'induction l.'}, {'name': 'trivial.'}, {'name': 'eauto.'}, {'name': 'unfold rev.', 'children': [{'name': 'intros.'}, {'name': 'induction l.'}, {'name': 'clear.'}, {'name': 'simpl.'}, {'name': 'destruct l.'}]}, {'name': 'pattern l.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.'}, {'name': 'idtac.'}, {'name': 'unfold rev.'}]}]}]}]}, {'name': 'destruct l.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.', 'children': [{'name': 'simpl.', 'children': [{'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'unfold app.'}]}, {'name': 'destruct l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}, {'name': 'try contradiction.'}]}]}, {'name': 'eauto.'}, {'name': 'try contradiction.'}]}]}]}]}]}, {'name': 'try discriminate.'}, {'name': 'try congruence.'}, {'name': 'destruct l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'eauto.'}, {'name': 'simpl.', 'children': [{'name': 'eauto.'}, {'name': 'try easy.'}, {'name': 'f_equal.'}, {'name': 'unfold rev.', 'children': [{'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'destruct l.'}, {'name': 'case l.'}]}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}]}]}, {'name': 'case l.', 'children': [{'name': 'eauto.', 'children': [{'name': 'intros.'}, {'name': 'induction l.'}, {'name': 'simpl.'}, {'name': 'trivial.'}, {'name': 'pattern l.'}, {'name': 'eauto.'}]}]}]}, {'name': 'unfold app.', 'children': [{'name': 'simpl.'}, {'name': 'unfold rev.', 'children': [{'name': 'unfold app.'}, {'name': 'simpl.'}, {'name': 'unfold rev.'}, {'name': 'eauto.'}, {'name': 'destruct l.'}, {'name': 'case l.'}]}, {'name': 'eauto.'}, {'name': 'unfold app.'}, {'name': 'case l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'intros.'}, {'name': 'simpl.'}, {'name': 'induction l.'}, {'name': 'trivial.'}, {'name': 'eauto.'}, {'name': 'unfold rev.'}, {'name': 'pattern l.'}]}]}, {'name': 'destruct l.', 'children': [{'name': 'simpl.'}, {'name': 'eauto.', 'children': [{'name': 'simpl.'}, {'name': 'destruct l.'}, {'name': 'eauto.'}, {'name': 'try contradiction.'}]}]}]}, {'name': 'intros.'}]}, {'name': 'try discriminate.'}, {'name': 'subst.'}, {'name': 'try easy.'}, {'name': 'try congruence.'}, {'name': 'intuition.'}, {'name': 'try reflexivity.'}]}]}, {'name': 'try contradiction.'}]}]}, {'name': 'simpl.'}, {'name': 'subst.'}]};
+var width = 960,
+    height = 1000;
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+var svg = d3.select("body").append("svg")
+    .attr("style","display:block; max-height: 100vh; object-fit: contain")
+    .attr("viewBox", "-75 0 " + (width+75) + " " + height)
+  .append("g")
+
+var i = 0,
+    duration = 750,
+    root;
+
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
+  }
+}
+
+var maxdepth = 0;
+
+
+
+
+function update(source) {
+
+  var treeData = treemap(root);
+
+  // Compute the new tree layout.
+  var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+  var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+  var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        return "translate(" + source.y0 + "," + source.x0 + ")";
+    })
+    .on('click', click);
+
+  // Add Circle for the nodes
+  nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 3)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  // Add labels for the nodes
+  nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13;
+      })
+      .attr("text-anchor", "middle", function(d) {
+          return d.children || d._children ? "end" : "start";
+      })
+      .text(function(d) { return d.data.name; });
+
+  // UPDATE
+  var nodeUpdate = nodeEnter.merge(node).call(function globalDepthUpdate(x) {
+    maxdepth = d3.max(svg.selectAll("g.node").data().map((x)=>x.depth));
+    if (maxdepth == undefined) {
+      maxdepth = 0;
+    }
+    nodes.forEach(function(d){ d.y = d.depth * Math.min(180,width/(maxdepth+2))});
+  })
+
+
+  // Transition to the proper position for the node
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) { 
+        return "translate(" + d.y + "," + d.x + ")";
+     });
+
+  // Update the node attributes and style
+  nodeUpdate.select('circle.node')
+  	.attr('r', 50)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+    })
+    .attr('cursor', 'pointer');
+
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+  // On exit reduce the node circles size to 0
+  nodeExit.select('circle')
+    .attr('r', 1e-6);
+
+  // On exit reduce the opacity of text labels
+  nodeExit.select('text')
+    .style('fill-opacity', 1e-6);
+
+  // ****************** links section ***************************
+
+  // Update the links...
+  var link = svg.selectAll('path.link')
+      .data(links, function(d) { return d.id; });
+
+  // Enter any new links at the parent's previous position.
+  var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', function(d){
+        var o = {x: source.x0, y: source.y0}
+        return diagonal(o, o)
+      });
+
+  // UPDATE
+  var linkUpdate = linkEnter.merge(link);
+
+  // Transition back to the parent element position
+  linkUpdate.transition()
+      .duration(duration)
+      .attr('d', function(d){ return diagonal(d, d.parent) });
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        var o = {x: source.x, y: source.y}
+        return diagonal(o, o)
+      })
+      .remove();
+
+  // Store the old positions for transition.
+  nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  // Creates a curved (diagonal) path from parent to the child nodes
+  function diagonal(s, d) {
+
+    path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+    return path
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+    update(d);
+  }
+  
+}
---
+++ static/zeqfive_tree.js
@@ -0,0 +1,186 @@
+var treeData = {'name': 'zeqfive', 'children': [{'name': 'intros.', 'children': [{'name': 'discriminate.', 'children': [{'name': 'QED'}]}]}]};
+var width = 960,
+    height = 1000;
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+var svg = d3.select("body").append("svg")
+    .attr("style","display:block; max-height: 100vh; object-fit: contain")
+    .attr("viewBox", "-75 0 " + (width+75) + " " + height)
+  .append("g")
+
+var i = 0,
+    duration = 750,
+    root;
+
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
+  }
+}
+
+var maxdepth = 0;
+
+
+
+
+function update(source) {
+
+  var treeData = treemap(root);
+
+  // Compute the new tree layout.
+  var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+  var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+  var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        return "translate(" + source.y0 + "," + source.x0 + ")";
+    })
+    .on('click', click);
+
+  // Add Circle for the nodes
+  nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 3)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  // Add labels for the nodes
+  nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13;
+      })
+      .attr("text-anchor", "middle", function(d) {
+          return d.children || d._children ? "end" : "start";
+      })
+      .text(function(d) { return d.data.name; });
+
+  // UPDATE
+  var nodeUpdate = nodeEnter.merge(node).call(function globalDepthUpdate(x) {
+    maxdepth = d3.max(svg.selectAll("g.node").data().map((x)=>x.depth));
+    if (maxdepth == undefined) {
+      maxdepth = 0;
+    }
+    nodes.forEach(function(d){ d.y = d.depth * Math.min(180,width/(maxdepth+2))});
+  })
+
+
+  // Transition to the proper position for the node
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) { 
+        return "translate(" + d.y + "," + d.x + ")";
+     });
+
+  // Update the node attributes and style
+  nodeUpdate.select('circle.node')
+  	.attr('r', 50)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+    })
+    .attr('cursor', 'pointer');
+
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+  // On exit reduce the node circles size to 0
+  nodeExit.select('circle')
+    .attr('r', 1e-6);
+
+  // On exit reduce the opacity of text labels
+  nodeExit.select('text')
+    .style('fill-opacity', 1e-6);
+
+  // ****************** links section ***************************
+
+  // Update the links...
+  var link = svg.selectAll('path.link')
+      .data(links, function(d) { return d.id; });
+
+  // Enter any new links at the parent's previous position.
+  var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', function(d){
+        var o = {x: source.x0, y: source.y0}
+        return diagonal(o, o)
+      });
+
+  // UPDATE
+  var linkUpdate = linkEnter.merge(link);
+
+  // Transition back to the parent element position
+  linkUpdate.transition()
+      .duration(duration)
+      .attr('d', function(d){ return diagonal(d, d.parent) });
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        var o = {x: source.x, y: source.y}
+        return diagonal(o, o)
+      })
+      .remove();
+
+  // Store the old positions for transition.
+  nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  // Creates a curved (diagonal) path from parent to the child nodes
+  function diagonal(s, d) {
+
+    path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+    return path
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+    update(d);
+  }
+  
+}
