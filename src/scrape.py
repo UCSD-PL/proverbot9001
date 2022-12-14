e7400d558e5c05feaaf0052072a06e1c04bab1ce
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2022-12-14 13:14:11 -0800

    Mark the end of scrape files so that you can run them when catted together

--- src/scrape.py
+++ src/scrape.py
@@ -127,6 +127,7 @@ def scrape_file(coqargs: List[str], args: argparse.Namespace,
                                         dynamic_ncols=True,
                                         bar_format=mybarfmt):
                         process_statement(args, coq, command, f)
+                    print("(* End of File *)", file=f)
                 shutil.move(temp_file, result_file)
                 return result_file
             except serapi_instance.TimeoutError:
