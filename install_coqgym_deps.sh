9b3bfed85eefecd68749650b1be5fef4c155c828
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-01-03 14:43:57 -0800

    Fix order of submodule commands

--- install_coqgym_deps.sh
+++ install_coqgym_deps.sh
@@ -10,7 +10,7 @@ then
     ~/.local/ruby-build 3.1.2 ~/.local/
 fi
 
-git submodule update && git submodule init
+git submodule init && git submodule update
 
 # Sync opam state to local
 rsync -av --delete $HOME/.opam.dir/ /tmp/${USER}_dot_opam | tqdm --desc="Reading shared opam state" > /dev/null
