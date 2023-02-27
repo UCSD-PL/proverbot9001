d595b6fb1c45eed4527652162a9500613ff20058
Alex Sanchez-Stern <sanchezstern@cs.umass.edu>
2023-02-27 17:01:18 -0500

    Print erroring files instead of messages for get-scrape-failures-coq-projects

--- get-scrape-failures-coq-projects.sh
+++ get-scrape-failures-coq-projects.sh
@@ -4,5 +4,5 @@ shopt -s globstar
 
 for project in $(jq -r '.[55:]|.[].project_name' coqgym_projs_splits.json); do
     ls coq-projects/$project/**/*.scrape > /dev/null
-    grep "[Ee]rr" coq-projects/$project/scrape-output.out
+    grep -l "[Ee]rr" coq-projects/$project/scrape-output.out
 done
