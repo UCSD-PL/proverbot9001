1cb9b36e60b87cad4d4cc7e62278569d4f548ab6
Saketh Kasibatla <sake.kasi@gmail.com>
2023-06-24 19:52:44 +0000

    Fix permission error with sed in place

--- src/patch_compcert.sh
+++ src/patch_compcert.sh
@@ -9,10 +9,14 @@ while [ -L "$src" ]; do
 done
 MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
 
-sed -i.bkp \
+# not using in place because it ran into permission issues
+# sed: preserving permissions for ‘/workspace/src/../CompCert/backend/seduqi2mA’: Permission denied
+sed \
     -e 's/\(destruct (\)\(eqb ro1 ro2) eqn:RO\.\.\.\)/\1Bool.\2/'\
     -e 's/\(destruct (\)\(eqb vo1 vo2) eqn:VO\.\.\.\)/\1Bool.\2/'\
-    $MYDIR/../CompCert/backend/Unusedglobproof.v
+    $MYDIR/../CompCert/backend/Unusedglobproof.v > $MYDIR/../CompCert/backend/Unusedglobproof.v.new
+
+mv $MYDIR/../CompCert/backend/Unusedglobproof.v.new $MYDIR/../CompCert/backend/Unusedglobproof.v
 
 gawk -i inplace -v INPLACE_SUFFIX=.bkp \
     '/Remark match_is_call_cont/ {in_target=1}
