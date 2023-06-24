d0dfa3d1c4e75c1ba66cb5fef191495d8b2a68b7
Saketh Kasibatla <sake.kasi@gmail.com>
2023-06-24 19:52:10 +0000

    Add automatic history tracking

---
+++ scripts/bash_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ~/.bashrc
+source $SCRIPT_DIRECTORY/track-history.bash
--- scripts/track-history.bash
+++ scripts/track-history.bash
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-log_bash_history()
-{
-  local rc=$?
-  local cwd=$(pwd)
-  local SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  local PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
+echo "tracking history"
 
-  if [[ "$cwd" == "$PROJECT_ROOT"* ]]; then
-    [[ "$(history 1)" =~ ^\ *[0-9]+\ +([^\ ]+\ [^\ ]+)\ +(.*)$ ]]
-    if [[ ${BASH_REMATCH[1]} != "${BASH_LAST_EXECUTED_COMMAND}" ]]; then
-      local LOG_FILE="$PROJECT_ROOT/.bash_history"
-      echo "$(date "+%Y-%m-%d.%H:%M:%S") $(hostname -s) [$$]: ${BASH_COMMAND}" >> "$LOG_FILE"
-      export BASH_LAST_EXECUTED_COMMAND="${BASH_REMATCH[1]}"
-    fi
-  fi
-  return $rc
+# I'm nervous about how bash's dynamic scoping affects this
+# it works if it's lexical scoping, which I'm more used to
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
+
+_bash_history_append()
+{
+  local LOG_FILE="$PROJECT_ROOT/.bash_history"
+  history -a $LOG_FILE
 }
 
-export PROMPT_COMMAND="log_bash_history"
+export PROMPT_COMMAND="_bash_history_append; $PROMPT_COMMAND"
