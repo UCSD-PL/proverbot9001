f332d3c6f93f91de898cc424a014d5102e10597c
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-06-28 13:37:13 -0700

    Change linearized version instead of original version of file

--- fix_proof_using_linearized.sh
+++ fix_proof_using_linearized.sh
@@ -4,7 +4,9 @@ set -e
 
 FILES=$(echo $(jq -r ".[]  | (.test_files[], .train_files[])" compcert_projs_splits.json))
 for file in $FILES; do
-    echo "Fixing "$file
-    python src/add_proof_using.py --prelude="./CompCert/" $file CompCert/$file.tmp
-    mv CompCert/{$file.tmp,$file}
+    echo "Fixing "$file.lin
+    HASH=$(head -n 1 CompCert/$file.lin)
+    python src/add_proof_using.py --prelude="./CompCert/" <(tail -n +2 CompCert/$file.lin) CompCert/$file.tmp
+    echo $HASH > CompCert/$file.lin
+    cat CompCert/$file.tmp >> CompCert/$file.lin
 done
