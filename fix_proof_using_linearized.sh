0adea0c52f98e7388154bdae7276d76777a50709
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-06-28 13:51:27 -0700

    Fix skipping code

--- fix_proof_using_linearized.sh
+++ fix_proof_using_linearized.sh
@@ -4,6 +4,10 @@ set -e
 
 FILES=$(echo $(jq -r ".[]  | (.test_files[], .train_files[])" compcert_projs_splits.json))
 for file in $FILES; do
+    if grep -q "Proof using" CompCert/$file.lin; then
+        echo "Skipping "$file.lin" (it appears to be already fixed)"
+        continue
+    fi
     echo "Fixing "$file.lin
     HASH=$(head -n 1 CompCert/$file.lin)
     python src/add_proof_using.py --prelude="./CompCert/" <(tail -n +2 CompCert/$file.lin) CompCert/$file.tmp
