c17f80731610fa45f471a6f4d6f593bd3995d1f1
zhannakaufma <zhannakaufma@umass.edu>
2023-07-24 07:11:24 +0000

    size max for cache

--- src/rl.py
+++ src/rl.py
@@ -16,6 +16,8 @@ from operator import itemgetter
 from typing import (List, Optional, Dict, Tuple, Union, Any, Set,
                     Sequence, TypeVar, Callable)
 
+from collections import OrderedDict
+
 from util import unwrap, eprint, print_time, nostderr
 
 with print_time("Importing torch"):
@@ -401,22 +403,37 @@ def read_tasks_file(args: argparse.Namespace, task_file: str, curriculum: bool):
 
 class CachedObligationEncoder(coq2vec.CoqContextVectorizer):
     obl_cache: Dict[Obligation, torch.FloatTensor]
+    max_size: int
     def __init__(self, term_encoder: 'coq2vec.CoqTermRNNVectorizer',
-                 max_num_hypotheses: int) -> None:
+            max_num_hypotheses: int, max_size: int=5000) -> None:
         super().__init__(term_encoder, max_num_hypotheses)
-        self.obl_cache = {}
+        self.obl_cache = OrderedDict()
+        self.max_size = max_size #TODO: Add in arguments if desired
     def obligations_to_vectors_cached(self, obls: List[Obligation]) \
             -> torch.FloatTensor:
         encoded_obl_size = self.term_encoder.hidden_size * (self.max_num_hypotheses + 1)
+
+        cached_results = []
+        for obl in obls:
+            r = self.obl_cache.get(obl, None)
+            if r is not None:
+                self.obl_cache.move_to_end(obl)
+            cached_results.append(r)
+
         encoded = run_network_with_cache(
             lambda x: self.obligations_to_vectors(x).view(len(x), encoded_obl_size),
             [coq2vec.Obligation(list(obl.hypotheses), obl.goal) for obl in obls],
-            [self.obl_cache.get(obl, None) for obl in obls])
+            cached_results)
+
         # for row, obl in zip(encoded, obls):
         #     assert obl not in self.obl_cache or (self.obl_cache[obl] == row).all(), \
         #         (self.obl_cache[obl] == row)
+
         for row, obl in zip(encoded, obls):
             self.obl_cache[obl] = row
+            self.obl_cache.move_to_end(obl)
+            if len(self.obl_cache) > self.max_size:
+                self.obl_cache.popitem(last=False)
         return encoded
 
 class VNetwork:
@@ -772,7 +789,7 @@ def evaluate_results(args: argparse.Namespace,
                      tasks: List[RLTask]) -> None:
     proofs_completed = 0
     for task in tasks:
-        if worker.evaluate_job(task.to_job(), tactic_prefix):
+        if worker.evaluate_job(task.to_job(), task.tactic_prefix):
             proofs_completed += 1
     print(f"{proofs_completed} out of {len(tasks)} "
           f"tasks successfully proven "
