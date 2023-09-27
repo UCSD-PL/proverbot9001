b66d90e2111e7656e0d3ad792cf15e386172aa19
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-09-27 19:33:37 +0000

    Remove old project skipping in getting scrape failures

--- get-scrape-failures-coq-projects.sh
+++ get-scrape-failures-coq-projects.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 shopt -s globstar
-
-for project in $(jq -r '.[55:]|.[].project_name' coqgym_projs_splits.json); do
+for project in $(jq -r '.[].project_name' coqgym_projs_splits.json); do
     ls coq-projects/$project/**/*.scrape > /dev/null
     grep -l "[Ee]rr" coq-projects/$project/scrape-output.out
 done
