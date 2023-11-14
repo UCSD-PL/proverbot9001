1526ebf82cd6049c1ecfede607f55456254a1a02
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-11-14 13:06:32 -0800

    Fixes and new features for step comparing scripts

--- src/compare_steps.py
+++ src/compare_steps.py
@@ -5,6 +5,9 @@ import os.path
 import json
 import csv
 from glob import glob
+from pathlib import Path
+
+from typing import List
 
 import coq_serapy
 
@@ -13,11 +16,17 @@ def main() -> None:
     parser.add_argument("reporta")
     parser.add_argument("reportb")
     parser.add_argument("--print-a-shorter", action='store_true')
+    parser.add_argument("--print-a-only", action="store_true")
     parser.add_argument("--print-b-shorter", action='store_true')
+    parser.add_argument("--print-b-only", action="store_true")
     parser.add_argument("--full-csv", default=None)
     parser.add_argument("--a-name", default="Report A")
     parser.add_argument("--b-name", default="Report B")
     args = parser.parse_args()
+
+    compare_steps(args)
+
+def compare_steps(args: argparse.Namespace):
 
     a_succ_steps = 0
     a_shorter = 0
@@ -40,7 +49,6 @@ def main() -> None:
                                    f"{args.a_name.lower()}_solution_length",
                                    f"{args.b_name.lower()}_steps_searched",
                                    f"{args.b_name.lower()}_solution_length"])
-            pass
 
     for filename_a in glob(os.path.join(args.reporta, "*-proofs.txt")):
         with open(filename_a, 'r') as f:
@@ -60,7 +68,7 @@ def main() -> None:
         for job, sol_a in proof_data_a:
             try:
                 job_b, sol_b = b_dict[(job[2], job[3])]
-                assert job_b == job, (job, job_b)
+                assert job_eq(job_b, job), (job, job_b)
             except KeyError:
                 print(f"Warning: couldn't find job {(job[2], job[3])} from file {filename_a} "
                       f"in filename {filename_b}")
@@ -102,9 +110,15 @@ def main() -> None:
                     b_proof_shorter += 1
             elif sol_b["status"] == "SUCCESS":
                 b_succ_not_a += 1
+                if args.print_b_only:
+                    print(f"For job {job[1]}:{job[2]}:{lemma_name}, "
+                          f"Only {args.b_name} succeeded.")
             #     print("Only second report succeeded")
             elif sol_a["status"] == "SUCCESS":
                 a_succ_not_b += 1
+                if args.print_a_only:
+                    print(f"For job {job[1]}:{job[2]}:{lemma_name}, "
+                          f"Only {args.a_name} succeeded.")
             #     print("Only first report succeeded")
 
     print(f"Total steps: {a_succ_steps} ({args.a_name}) vs {b_succ_steps} ({args.b_name})")
@@ -118,6 +132,14 @@ def main() -> None:
     print(f"{a_succ_not_b} proofs where {args.a_name} succeeded but {args.b_name} did not.")
     print(f"{b_succ_not_a} proofs where {args.b_name} succeeded but {args.a_name} did not.")
     print(f"{both_succ} proofs where both succeeded")
+
+def job_eq(job1: List[str], job2: List[str]) -> bool:
+    assert len(job1) == 4
+    assert len(job2) == 4
+    return job1[0] == job2[0] and \
+            Path(job1[1]) == Path(job2[1]) and \
+            job1[2] == job2[2] and \
+            job1[3] == job2[3]
 
 if __name__ == "__main__":
     main()
