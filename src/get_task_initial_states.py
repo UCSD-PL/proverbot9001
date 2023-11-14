a67b19cf71817bc41fd21ab638e1b7b99b56743a
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-11-14 20:52:31 +0000

    Add resuming functionality to state-getter script

--- src/get_task_initial_states.py
+++ src/get_task_initial_states.py
@@ -9,12 +9,14 @@ from coq_serapy import Obligation
 
 from gen_rl_tasks import RLTask
 from rl import FileReinforcementWorker
+from util import eprint
 
 def main() -> None:
   argparser = argparse.ArgumentParser()
   argparser.add_argument("-v", "--verbose", action='count', default=0)
   argparser.add_argument("--print-timings", action='store_true')
   argparser.add_argument("--prelude", default=".", type=Path)
+  argparser.add_argument("--no-resume", action='store_false', dest="resume")
   argparser.add_argument("tasks_file", type=Path)
   argparser.add_argument("output_file", type=Path)
   args = argparser.parse_args()
@@ -23,21 +25,23 @@ def main() -> None:
 
   with args.tasks_file.open("r") as f:
     tasks = [RLTask(**json.loads(line)) for line in f]
-  obls = get_starting_obls(args, tasks)
-  with args.output_file.open("w") as f:
-    for obl, task in zip(obls, tasks):
-      print(json.dumps((task.as_dict(), obl.to_dict())), file=f)
-  
-def get_starting_obls(args: argparse.Namespace, tasks: List[RLTask])\
-   -> List[Obligation]:
-  results: List[Obligation] = []
-  with FileReinforcementWorker(args, None) as worker:
-    for task in tqdm(tasks, desc="Retrieving proof states"):
-      worker.run_into_task(task.to_job(), task.tactic_prefix)
-      results.append(worker.coq.proof_context.fg_goals[0])
-  return results
-
-  pass
+  if args.output_file.exists():
+    if args.resume:
+      with args.output_file.open('r') as f:
+        done_tasks = [RLTask(**task_dict) for l in f
+                      for task_dict, obl_dict in (json.loads(l),)]
+      tasks = list(set(tasks).difference(set(done_tasks)))
+      eprint("Resumed from file {str(args.output_file)}")
+    else:
+      with args.output_file.open("w") as f:
+        pass
+  with args.output_file.open("a") as f:
+    with FileReinforcementWorker(args, None) as worker:
+      for task in tqdm(tasks, desc="Retrieving proof states"):
+        worker.run_into_task(task.to_job(), task.tactic_prefix)
+        print(json.dumps((
+          task.as_dict(),
+          worker.coq.proof_context.fg_goals[0].to_dict())), file=f)
 
 if __name__ == "__main__":
   main()
