2e866536af2751f6db2ec0daad6f685d103624be
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-11-27 19:57:59 +0000

    Better resuming bar and new worker for each file in getting obls

--- src/get_task_initial_states.py
+++ src/get_task_initial_states.py
@@ -30,18 +30,32 @@ def main() -> None:
       with args.output_file.open('r') as f:
         done_tasks = [RLTask(**task_dict) for l in f
                       for task_dict, obl_dict in (json.loads(l),)]
+      start_bar_from = len(done_tasks)
       tasks = list(set(tasks).difference(set(done_tasks)))
       eprint(f"Resumed from file {str(args.output_file)}")
     else:
       with args.output_file.open("w") as f:
         pass
+      start_bar_from = 0
   with args.output_file.open("a") as f:
-    with FileReinforcementWorker(args, None) as worker:
-      for task in tqdm(tasks, desc="Retrieving proof states"):
-        worker.run_into_task(task.to_job(), task.tactic_prefix)
-        print(json.dumps((
-          task.as_dict(),
-          worker.coq.proof_context.fg_goals[0].to_dict())), file=f)
+    cur_project = ""
+    cur_file = ""
+    tasks_left = tasks[1:]
+    next_task = tasks[0]
+    with tqdm(desc="Retrieving proof states",
+              initial=start_bar_from,
+              total=len(tasks) + start_bar_from,
+              dynamic_ncols=True) as bar:
+      while len(tasks_left) > 0:
+        with FileReinforcementWorker(args, None) as worker:
+          while next_task.project == cur_project and \
+                next_task.src_file == cur_file:
+            worker.run_into_task(next_task.to_job(), next_task.tactic_prefix)
+            print(json.dumps((
+              next_task.as_dict(),
+              worker.coq.proof_context.fg_goals[0].to_dict())), file=f)
+            next_task = tasks_left.pop(0)
+            bar.update()
 
 if __name__ == "__main__":
   main()
