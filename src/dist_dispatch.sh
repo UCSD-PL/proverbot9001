f3a5ad14823d16ea302b8103c97eef755acfda07
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2023-12-04 20:00:37 +0000

    Add dist dispatch file

---
+++ src/dist_dispatch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd $SLURM_SUBMIT_DIR
+if (( $SLURM_NODEID == 0 )) ; then
+  python src/distributed_rl_learning_server.py $(eval echo $2) &> $1/learner.out
+else
+  python src/distributed_rl_acting_worker.py $(eval echo $3) &> $1/actor-$((${SLURM_NODEID}-1)).out
+fi
