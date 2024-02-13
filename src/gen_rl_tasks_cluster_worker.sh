92a7c7bac0f3e2230a657c7253604ad9791173a9
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-02-13 22:32:19 +0000

    Revert bad merge stuff

--- src/gen_rl_tasks_cluster_worker.sh
+++ src/gen_rl_tasks_cluster_worker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CUR_DIR=/work/pi_brun_umass_edu/asanchezster/proverbot9001/src/
+CUR_DIR=$HOME/work/proverbot9001/src/
 
 python src/gen_rl_tasks_cluster_worker.py $@
--- src/search_file_cluster_worker.sh
+++ src/search_file_cluster_worker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CUR_DIR=/work/pi_brun_umass_edu/asanchezster/proverbot9001/src/
+CUR_DIR=$HOME/work/proverbot9001/src/
 
 python3 $CUR_DIR/search_file_cluster_worker.py $@
