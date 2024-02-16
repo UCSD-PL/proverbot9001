44bbd13a1e987891fd92ae86607f8acc50aa4de2
Abhishek Varghese <avarghese@umass.edu>
2024-02-16 23:51:47 +0000

    Added all reports comparision. Run after all the report generations are finished

---
+++ src/reports_post_report_analysis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Analyzing reports"
+
+
+folders_string=$(find output/ -name "*-search-report*")
+echo -n > output/rl/Compare_reports.txt
+
+folders=()
+for folder in $folders_string; do
+    folders+=($folder)
+done
+
+#LLMs are amazing at debugging bash code
+for ((i=0; i<${#folders[@]}; i++)); do
+    for ((j=i+1; j<${#folders[@]}; j++)); do
+        folder1="${folders[i]}"
+        folder2="${folders[j]}"
+        printf "\n --------- Comparing ${folder1} with ${folder2} --------------- \n" >> output/rl/Compare_reports.txt
+        srun python src/compare_steps.py "$folder1" "$folder2" >> output/rl/Compare_reports.txt
+    done
+done
