b717b08375299b538f502a74405c6b9485fc0b3b
Abhishek Varghese <avarghese@umass.edu>
2024-02-16 20:22:52 +0000

    Added different search depths

--- src/reports_makereport_search_baselines.sh
+++ src/reports_makereport_search_baselines.sh
@@ -16,6 +16,14 @@ python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prel
 
 python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --search-type=dfs --output=output/rl/results/DFS-search-report_1024 --max-steps=1024
 
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --search-type=dfs --output=output/rl/results/DFS-search-report_ms128_dep4 --max-steps=128 --search-depth=4
+
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --search-type=dfs --output=output/rl/results/DFS-search-report_ms128_dep6 --max-steps=128 --search-depth=6
+
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --search-type=dfs --output=output/rl/results/DFS-search-report_ms128_dep8 --max-steps=128 --search-depth=8
+
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --search-type=dfs --output=output/rl/results/DFS-search-report_ms128_dep2 --max-steps=128 --search-depth=2
+
 python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=const --search-type=astar --output=output/rl/results/Astar-search-report  --max-steps=128
 
 python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=certainty --search-type=best-first --output=output/rl/results/BestFirst-search-report_cert  --max-steps=128
