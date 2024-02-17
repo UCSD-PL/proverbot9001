07ba211a7bade06ccab10ada6deb5aae85b1b77e
Abhishek Varghese <avarghese@umass.edu>
2024-02-17 23:50:16 +0000

    changed hyperparameters

--- src/reports_prepare_rl_pretrain.sh
+++ src/reports_prepare_rl_pretrain.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gamma=0.7
+gamma=0.9
 train_path=data/rl_debug_jobs.json
 coq2vecweightspath=data/term2vec-weights-59.dat
 
@@ -9,10 +9,10 @@ eval $(opam env)
 
 echo "Running Pretraining"
 
-python src/distributed_get_task_initial_states.py $train_path output/rl/initial_states.json --state-dir output/rl/state_dirs/prepare_init_state --num-workers 32 
+python src/distributed_get_task_initial_states.py $train_path output/rl/initial_states.json --state-dir output/rl/state_dirs/prepare_init_state --num-workers 32 --resume 
 
 srun --pty -p gpu --mem=8G --time=1:00:00 --gres=gpu:1 python src/supervised_v_values.py \
         --encoder-weights=$coq2vecweightspath -o output/rl/pretrained_weights.pkl output/rl/initial_states.json \
-        --prelude=CompCert --mode=train --supervised-weights=data/polyarg-weights-develop.dat -l 7e-4 --learning-rate-decay=.57 \
+        --prelude=CompCert --mode=train --supervised-weights=data/polyarg-weights-develop.dat -l 7e-7 --learning-rate-decay=.57 \
         --learning-rate-step=80 --gamma=$gamma --num-epochs=200
 
--- src/reports_train_rl.sh
+++ src/reports_train_rl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gamma=0.7
+gamma=0.9
 train_path=data/rl_debug_jobs.json
 coq2vecweightspath=data/term2vec-weights-59.dat
 
@@ -11,7 +11,7 @@ echo "Running RL training"
 python src/distributed_rl.py --mem=16G --num-actors=32 --supervised-weights=data/polyarg-weights-develop.dat \
              --coq2vec-weights=$coq2vecweightspath compcert_projs_splits.json --prelude=./CompCert  \
               --backend=serapi --gamma=$gamma -s5 -p5 --learning-rate=0.000005 -n7 -o output/rl/final_trained_weights_rl.pkl \
-              --tasks-file=$train_path --resume=no -b 1024 --allow-partial-batches --sync-target-every=128 \
+              --tasks-file=$train_path --resume=yes -b 1024 --allow-partial-batches --sync-target-every=128 \
                --state-dir=output/rl/state_dirs/drl --partition gpu -v --start-from output/rl/pretrained_weights.pkl
 
 
