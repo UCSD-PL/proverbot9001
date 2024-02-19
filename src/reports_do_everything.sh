cb83f02f25eefbb1f92e50d5f2d13172692059ff
Abhishek Varghese <avarghese@umass.edu>
2024-02-19 19:43:35 +0000

    Added CoqGym

--- src/reports_do_everything.sh
+++ src/reports_do_everything.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+
+task_path=$2
+project=$1
+
+if [ -z "$3" ]; then
+    resume=""
+else
+    if [ $3 == "resume" ]; then
+        resume="resume"
+    else 
+        resume="no-resume"
+    fi
+fi
+
 module load opam/2.1.2 graphviz/2.49.0+py3.8.12 openmpi/4.1.3+cuda11.6.2
 eval $(opam env)
 
 
-bash src/reports_prepare_rl_pretrain.sh
-bash src/reports_train_rl.sh
+bash src/reports_prepare_rl_pretrain.sh $1 $2
+bash src/reports_train_rl.sh $1 $2 $resume
+
 bash src/reports_makereport_search_rlweights.sh
 
 bash src/reports_makereport_search_baselines.sh
--- src/reports_train_rl.sh
+++ src/reports_train_rl.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+train_path=$2
+if [ $1 == "CompCert" ]; then
+    proj_split_title="compcert_projs_splits.json"
+    prelude="CompCert"
+elif [ $1 == "CoqGym" ]; then
+    proj_split_title=coqgym_projs_splits.json
+    prelude=coq-projects
+fi
+
+if [ -z "$3" ]; then
+    resume="no"
+else
+    if [ $3 == "resume" ]; then
+        resume="yes"
+    else 
+        resume="no"
+    fi
+fi
+
 gamma=0.9
-train_path=data/rl_debug_jobs.json
 coq2vecweightspath=data/term2vec-weights-59.dat
 
 eval $(opam env)
@@ -9,9 +27,9 @@ eval $(opam env)
 echo "Running RL training"
 
 python src/distributed_rl.py --mem=16G --num-actors=32 --supervised-weights=data/polyarg-weights-develop.dat \
-             --coq2vec-weights=$coq2vecweightspath compcert_projs_splits.json --prelude=./CompCert  \
+             --coq2vec-weights=$coq2vecweightspath $proj_split_title --prelude=./$prelude  \
               --backend=serapi --gamma=$gamma -s5 -p5 --learning-rate=0.000005 -n7 -o output/rl/final_trained_weights_rl.pkl \
-              --tasks-file=$train_path --resume=yes -b 1024 --allow-partial-batches --sync-target-every=128 \
+              --tasks-file=$train_path --resume=$resume -b 1024 --allow-partial-batches --sync-target-every=128 \
                --state-dir=output/rl/state_dirs/drl --partition gpu -v --start-from output/rl/pretrained_weights.pkl
 
 
