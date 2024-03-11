a30b1ca7fde07fbab02efa16e9654bb97706cf61
Abhishek Varghese <avarghese@umass.edu>
2024-03-11 21:11:17 +0000

    Correected names for astar and bfs reports

--- src/reports_makereport_search_rlweights.sh
+++ src/reports_makereport_search_rlweights.sh
@@ -5,6 +5,6 @@ eval $(opam env)
 
 echo "Running rl searches"
 
-python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=best-first --pickled-estimator=output/rl/final_trained_weights_rl_pickle.pkl --output=output/rl/results/Astar_rl-search-report --max-steps=128
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=best-first --pickled-estimator=output/rl/final_trained_weights_rl_pickle.pkl --output=output/rl/results/BestFirst_rl-search-report --max-steps=128
 
-python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=output/rl/final_trained_weights_rl_pickle.pkl --output=output/rl/results/BestFirst_rl-search-report --max-steps=128
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=output/rl/final_trained_weights_rl_pickle.pkl --output=output/rl/results/Astar_rl-search-report --max-steps=128
