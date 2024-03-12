a6b0190c88888007f31ec123f3ecbc82ed2ad87d
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-03-12 19:39:02 +0000

    Make script more rrobust and flexible

--- src/reports_makereport_search_rlweights.sh
+++ src/reports_makereport_search_rlweights.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
+[ $# -ne 0 ] || (echo "Not enough arguments" &>2 && exit 2)
+
 job_name=$1
-eval $(opam env)
+ESTIMATOR_WEIGHTS=${2:-output/rl_$job_name/final_trained_weights_rl_pickle.pkl}
 
 echo "Running rl searches"
 
-python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=best-first --pickled-estimator=output/rl_$job_name/final_trained_weights_rl_pickle.pkl --output=output/rl_$job_name/results/BestFirst_rl-search-report --max-steps=128
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=best-first --pickled-estimator=$ESTIMATOR_WEIGHTS --output=output/rl_$job_name/results/BestFirst_rl-search-report --max-steps=128
 
-python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=output/rl_$job_name/final_trained_weights_rl_pickle.pkl --output=output/rl_$job_name/results/Astar_rl-search-report --max-steps=128
+python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=$ESTIMATOR_WEIGHTS --output=output/rl_$job_name/results/Astar_rl-search-report --max-steps=128
