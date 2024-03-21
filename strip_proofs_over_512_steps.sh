4d298b83fd84aa71f5af02eac2d0170b1527ff6f
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-03-21 19:29:40 +0000

    Make the strip_proofs_over_513_steps.sh script non-destructive

--- strip_proofs_over_512_steps.sh
+++ strip_proofs_over_512_steps.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-echo "This script will OVERWRITE the proofs files in the report you give it. Please interrupt if this isn't what you want; otherwise, hit enter"
-read throwaway
-for proofsfile in $(find $1 -name "*-proofs.txt"); do
+[[ $# -ne 2 ]] && echo "Wrong number of arguments $# (2 expected)" && exit 1
+[ -d $2 ] && echo "Output dir already exists" && exit 1
+rsync -avzz --include="*/" --include="*-proofs.txt" --exclude="*" $1 $2
+for proofsfile in $(find $2 -name "*-proofs.txt"); do
     cat $proofsfile | jq -c ".[1].status = if .[1].steps_taken > 512 and .[1].status == \"SUCCESS\" then \"INCOMPLETE\" else .[1].status end" > $proofsfile.fixed
     mv $proofsfile{.fixed,}
 done
