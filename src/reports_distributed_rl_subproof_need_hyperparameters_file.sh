f330a124ae53000ac6c511c11d2fbbe1bd6b56bb
Abhishek Varghese <avarghese@umass.edu>
2024-04-24 18:06:02 +0000

    Start from goes to a new line

--- src/reports_distributed_rl_subproof_need_hyperparameters_file.sh
+++ src/reports_distributed_rl_subproof_need_hyperparameters_file.sh
@@ -41,12 +41,12 @@ else
 fi 
 
 
-python src/distributed_get_task_initial_states.py output/rl_$job_name/compcert_train_tasks_paper_filtered.json output/rl_$job_name/initial_states.json --state-dir output/rl_$job_name/state_dirs/prepare_init_state --num-workers 32 --resume --prelude=./$prelude
+# python src/distributed_get_task_initial_states.py output/rl_$job_name/compcert_train_tasks_paper_filtered.json output/rl_$job_name/initial_states.json --state-dir output/rl_$job_name/state_dirs/prepare_init_state --num-workers 32 --resume --prelude=./$prelude
 
-srun --pty -p gpu --mem=8G --time=8:00:00 --gres=gpu:1 python src/supervised_v_values.py \
-        --encoder-weights=$coq2vecweightspath -o output/rl_$job_name/pretrained_weights.pkl output/rl_$job_name/initial_states.json \
-        --prelude=CompCert --mode=train --supervised-weights=data/polyarg-weights-develop.dat -l $lr --learning-rate-decay=$lr_decay \
-        --learning-rate-step=$lr_step   --gamma=$gamma --num-epochs=1000 --hidden-size $hidden_size --num-layers $nlayers --batch-size=20
+# srun --pty -p gpu --mem=8G --time=8:00:00 --gres=gpu:1 python src/supervised_v_values.py \
+#         --encoder-weights=$coq2vecweightspath -o output/rl_$job_name/pretrained_weights.pkl output/rl_$job_name/initial_states.json \
+#         --prelude=CompCert --mode=train --supervised-weights=data/polyarg-weights-develop.dat -l $lr --learning-rate-decay=$lr_decay \
+#         --learning-rate-step=$lr_step   --gamma=$gamma --num-epochs=1000 --hidden-size $hidden_size --num-layers $nlayers --batch-size=20
 
 
 
@@ -55,16 +55,17 @@ python src/distributed_rl.py --mem=16G --num-actors=16 --supervised-weights=data
               --backend=serapi --gamma=$gamma -s$stepspereps -p$predictor_width --learning-rate=$lr -n$nepisodes -o output/rl_$job_name/final_trained_weights_rl.pkl \
               --tasks-file=output/rl_$job_name/compcert_train_tasks_paper_filtered.json \
                --resume=$resume -b 1024 --allow-partial-batches --sync-target-every=$sync_target \
-               --state-dir=output/rl_$job_name/state_dirs/drl --partition gpu-preempt -v --start-from output/rl_$job_name/pretrained_weights.pkl --hidden-size $hidden_size \
-            --num-layers $nlayers --learning-rate-step=$lr_step  --learning-rate-decay=$lr_decay --verifyv-every=128
+               --state-dir=output/rl_$job_name/state_dirs/drl --partition gpu-preempt -v --hidden-size $hidden_size \
+            --num-layers $nlayers --learning-rate-step=$lr_step  --learning-rate-decay=$lr_decay --verifyv-every=128 \
+            # --start-from output/rl_$job_name/pretrained_weights.pkl 
 
-python src/distributed_rl_eval.py --mem=16G --num-actors=8 --supervised-weights=data/polyarg-weights-develop.dat \
-        --coq2vec-weights=$coq2vecweightspath $proj_split_title --prelude=./$prelude  \
-        --backend=serapi --gamma=$gamma --rl-weights=$ESTIMATOR_WEIGHTS --tasks-file=data/rl_test_jobs.json -p16 --partition=gpu \
-        --evaluate --state-dir=output/rl_$job_name/state_dirs/drl_eval
+# python src/distributed_rl_eval.py --mem=16G --num-actors=8 --supervised-weights=data/polyarg-weights-develop.dat \
+#         --coq2vec-weights=$coq2vecweightspath $proj_split_title --prelude=./$prelude  \
+#         --backend=serapi --gamma=$gamma --rl-weights=$ESTIMATOR_WEIGHTS --tasks-file=data/rl_test_jobs.json -p16 --partition=gpu \
+#         --evaluate --state-dir=output/rl_$job_name/state_dirs/drl_eval
 
 
-srun python src/rl_to_pickle.py output/rl_$job_name/final_trained_weights_rl.pkl output/rl_$job_name/final_trained_weights_rl_pickle.pkl
+# srun python src/rl_to_pickle.py output/rl_$job_name/final_trained_weights_rl.pkl output/rl_$job_name/final_trained_weights_rl_pickle.pkl
 
-ESTIMATOR_WEIGHTS="output/rl_$job_name/final_trained_weights_rl_pickle.pkl"
-python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=$ESTIMATOR_WEIGHTS --output=output/rl_$job_name/results/Astar_rl-search-report --max-steps=128 --search-width=$width
+# ESTIMATOR_WEIGHTS="output/rl_$job_name/final_trained_weights_rl_pickle.pkl"
+# python ./src/search_file_cluster.py -p cpu --mem=8G --hard-depth-limit=20 --prelude=./CompCert --num-workers=32 --weightsfile=data/polyarg-weights-develop.dat -j1 compcert_projs_splits.json --scoring-function=pickled --search-type=astar --pickled-estimator=$ESTIMATOR_WEIGHTS --output=output/rl_$job_name/results/Astar_rl-search-report --max-steps=128 --search-width=$width
