3ecddd75de37d97ee45ca88e8c2b4678157d06d2
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2024-05-16 12:48:30 -0700

    Make sure proofs always start with a "Proof" command during search

--- src/search_worker.py
+++ src/search_worker.py
@@ -520,6 +520,8 @@ def attempt_search(args: argparse.Namespace,
     if "Proof" not in coq.prev_tactics[-1]:
         coq.run_stmt("Proof.")
     global obl_num
+    if "Proof" not in coq.prev_tactics[-1]:
+        coq.run_stmt("Proof.")
     if module_name:
         module_prefix = escape_lemma_name(module_name)
     else:
