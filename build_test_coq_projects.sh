36f02e849a31c187fd7249bbc37b255ee48ded67
zhannakaufma <zhannakaufma@umass.edu>
2026-01-09 04:31:47 +0000

    build and scrape partial

---
+++ build_test_coq_projects.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# source swarm-prelude.sh
+
+INIT_CMD="~/opam-scripts/read-opam.sh"
+
+NTHREADS=1
+while getopts ":j:" opt; do
+  case "$opt" in
+    j)
+      NTHREADS="${OPTARG}"
+      shift
+      ;;
+  esac
+done
+
+# Make sure ruby is in the path
+export PATH=$HOME/.local/bin:$PATH
+
+git submodule init && git submodule update
+
+TARGETS=${@:-$(jq -r '.[] | select(.test_files | length > 0) | .project_name' coqgym_projs_splits.json)}
+for project in $TARGETS; do
+    SBATCH_FLAGS=""
+
+    echo "#!/usr/bin/env bash" > coq-projects/$project/make.sh
+    echo ${INIT_CMD} >> coq-projects/$project/make.sh
+    if $(jq -e ".[] | select(.project_name == \"$project\") | has(\"build_command\")" \
+         coqgym_projs_splits.json); then
+        BUILD=$(jq -r ".[] | select(.project_name == \"$project\") | .build_command" \
+                   coqgym_projs_splits.json)
+    else
+        BUILD="make"
+    fi
+
+    if $(jq -e ".[] | select(.project_name == \"$project\") | has(\"build_partition\")" \
+            coqgym_projs_splits.json); then
+        PART=$(jq -r ".[] | select(.project_name == \"$project\") | .build_partition" \
+                   coqgym_projs_splits.json)
+        SBATCH_FLAGS+=" -p $PART"
+    fi
+
+    if $(jq -e ".[] | select(.project_name == \"$project\") | has(\"timeout\")" \
+         coqgym_projs_splits.json); then
+        TIMEOUT=$(jq -r ".[] | select(.project_name == \"$project\") | .timeout" \
+                     coqgym_projs_splits.json)
+        SBATCH_FLAGS+=" --time=${TIMEOUT}"
+    fi
+
+    SWITCH=$(jq -r ".[] | select(.project_name == \"$project\") | .switch" coqgym_projs_splits.json)
+
+    echo "eval \"$(opam env --set-switch --switch=$SWITCH)\"" >> coq-projects/$project/make.sh
+
+    echo "$BUILD -j $NTHREADS" >> coq-projects/$project/make.sh
+    chmod u+x coq-projects/$project/make.sh
+    (cd coq-projects/$project && sbatch --cpus-per-task=${NTHREADS} $SBATCH_FLAGS -o build-output.out make.sh)
+done
---
+++ scrape_test_coq_projects.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+
+INIT_CMD="~/opam-scripts/read-opam.sh"
+
+NTHREADS=1
+while getopts ":j:" opt; do
+  case "$opt" in
+    j)
+      NTHREADS="${OPTARG}"
+      shift
+      ;;
+  esac
+done
+
+# Make sure ruby is in the path
+export PATH=$HOME/.local/bin:$PATH
+
+TARGETS=${@:-$(jq -r '.[] | select(.test_files | length > 0) | .project_name' coqgym_projs_splits.json)}
+
+for project in $TARGETS; do
+    SWITCH=$(jq -r ".[] | select(.project_name == \"$project\") | .switch" coqgym_projs_splits.json)
+    if $(jq -e ".[] | select(.project_name == \"$project\") | has(\"prelude\")" \
+         coqgym_projs_splits.json); then
+        PRELUDE=$(jq -r ".[] | select(.project_name == \"$project\") | .prelude" coqgym_projs_splits.json)
+        echo "Prelude is $PRELUDE"
+    else
+        PRELUDE=""
+        echo "No prelude"
+    fi
+    FILES=$(echo $(jq -r ".[] | select(.project_name == \"$project\") | (.test_files[], .train_files[])" coqgym_projs_splits.json))
+    if [ -z "$FILES" ]; then
+        continue
+    fi
+
+    echo "#!/usr/bin/env bash" > coq-projects/$project/scrape.sh
+    echo ${INIT_CMD} >> coq-projects/$project/scrape.sh
+
+    echo "eval \"$(opam env --set-switch --switch=$SWITCH)\"" >> coq-projects/$project/scrape.sh
+
+    echo "./src/scrape.py -c --prelude=./coq-projects/$project/$PRELUDE ${FILES} > /dev/null" >> coq-projects/$project/scrape.sh
+    chmod u+x coq-projects/$project/scrape.sh
+    set -x
+    ./src/sbatch-retry.sh --cpus-per-task=${NTHREADS} -o "coq-projects/$project/scrape-output.out" "coq-projects/$project/scrape.sh"
+    set +x
+done
